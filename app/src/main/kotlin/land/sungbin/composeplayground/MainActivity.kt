c87ca5361e974aec075f25764cb9fbb38b7092bf
jisungbin <ji@sungb.in>
2022-03-09 20:47:29 +0900

    playgrounding

--- app/src/main/kotlin/land/sungbin/composeplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/composeplayground/MainActivity.kt
@@ -3,6 +3,7 @@ package land.sungbin.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
@@ -12,10 +13,30 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val vm: TestViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        lifecycleScope.launchWhenCreated {
+            launch {
+                vm.sharedFlow.collect {
+                    println("SharedFlow: $it")
+                }
+            }
+            launch {
+                vm.channelFlow.collect {
+                    println("ChannelFlow: $it")
+                }
+            }
+        }
+
         setContent {
             Text(
                 modifier = Modifier
