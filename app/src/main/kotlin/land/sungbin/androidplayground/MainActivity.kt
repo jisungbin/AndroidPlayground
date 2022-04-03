53f392147f6cbb545b3a7e8f483a2ab0e0a2882b
jisungbin <ji@sungb.in>
2022-04-03 16:33:39 +0900

    test toss-style snackbar

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -1,19 +1,29 @@
 package land.sungbin.androidplayground
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
-import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
 
-    private val vm: TestViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.rangeSlider.setValues(20f, 50f)
+
+        binding.tvToast.text = getString(R.string.app_name)
+        binding.tvToast.setTextColor(Color.YELLOW)
+        binding.cvSnackbarContainer.setCardBackgroundColor(Color.GRAY)
+        binding.cvSnackbarContainer.animate().run {
+            translationY(-500f)
+            interpolator = OvershootInterpolator()
+            duration = 1000L
+            withLayer()
+            start()
+        }
     }
 }
