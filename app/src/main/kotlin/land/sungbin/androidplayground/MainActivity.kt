6e16ad881157bc86f4c5d1e99e29818b90b2ef01
jisungbin <ji@sungb.in>
2022-05-15 20:19:54 +0900

    Test groups

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -5,33 +5,35 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.key
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box( // inline function
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                RecompositionGenerator()
-            }
+            Foo(false, "")
         }
     }
 
     @Composable
+    fun Foo(tf: Boolean, text: String) {
+        when (tf) {
+            true -> {
+                key(1, 2) {
+                    Text(text = text)
+                }
+            }
+            else -> {
+                key(1, 2) {
+                    Text(text = text)
+                }
+            }
+        }
+    }
+
+    /*@Composable
 //    @NonRestartableComposable
     private fun RecompositionGenerator() {
         SideEffect {
@@ -63,7 +65,7 @@ class MainActivity : ComponentActivity() {
         // [re-composition]
         // Composition: Button scope
         // Composition: Text function
-        /*Button(onClick = { number++ }) {
+        *//*Button(onClick = { number++ }) {
             SideEffect {
                 println("Composition: Button scope")
             }
@@ -71,7 +73,7 @@ class MainActivity : ComponentActivity() {
                 text = number.toString(),
                 logMessage = "Composition: Text function"
             )
-        }*/
+        }*//*
     }
 
     @Composable
@@ -85,7 +87,7 @@ class MainActivity : ComponentActivity() {
             println(logMessage)
         }
         Text(modifier = modifier, text = text)
-    }
+    }*/
 }
 
 /*
