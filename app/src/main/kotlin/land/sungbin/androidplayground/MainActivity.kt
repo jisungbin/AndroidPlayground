3ea3e0d3c528a169a23f1a8f7745ce49d42380d1
jisungbin <ji@sungb.in>
2022-05-17 22:48:59 +0900

    Test Recomposition

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -5,36 +5,23 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.TextField
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentRecomposeScope
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.SideEffect
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                TextFieldTest(text = stringResource(R.string.app_name))
-                currentRecomposeScope.invalidate()
-            }
+            Test()
         }
     }
 
     @Composable
-    private fun TextFieldTest(text: String) {
-        var textState by remember { mutableStateOf(text) }
-        TextField(value = textState, onValueChange = { textState = it })
+    fun Test() {
+        SideEffect {
+            println("AA")
+        }
+        Text("")
     }
 }
