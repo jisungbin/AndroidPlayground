5b99e1a73031ddc8be95c4c3b3ae7c77e85c62fb
jisungbin <ji@sungb.in>
2022-05-21 20:26:42 +0900

    Test invalidate

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -5,27 +5,44 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.currentRecomposeScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test()
+            Box(
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+            ) {
+                Test()
+            }
         }
     }
 
     @Composable
     fun Test() {
+        val currentRecomposeScope = currentRecomposeScope
         LaunchedEffect(Unit) {
             println("LaunchedEffect")
         }
         SideEffect {
             println("SideEffect")
         }
-        Text("")
+        Button(
+            onClick = {
+                currentRecomposeScope.invalidate()
+            }
+        ) {
+            Text(text = "invalidate")
+        }
     }
 }
