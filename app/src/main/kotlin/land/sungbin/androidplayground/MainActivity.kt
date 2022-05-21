e04fa1f6a59a2c29c2931049cd27c0da9edfb8f3
jisungbin <ji@sungb.in>
2022-05-22 04:38:15 +0900

    Test use State in Android View system

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -4,45 +4,81 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.currentRecomposeScope
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.databinding.DataBindingUtil
+import land.sungbin.androidplayground.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
+
+    private var state by mutableStateOf(1)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.tvLabel.text = "State: $state"
+        binding.btnTest.setOnClickListener {
+            state++
+        }
+
+        /*setContent {
             Box(
-                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
             ) {
                 Test()
             }
+        }*/
+    }
+
+    @Composable
+    private fun Test() {
+        var text by remember { mutableStateOf("") }
+        SideEffect {
+            println("Recomposed Test")
+        }
+        LoggingButton(onClick = { text = "$text\n$text" }) {
+            LoggingText(text = text)
         }
     }
 
     @Composable
-    fun Test() {
-        val currentRecomposeScope = currentRecomposeScope
-        LaunchedEffect(Unit) {
-            println("LaunchedEffect")
-        }
+    private fun LoggingButton(
+        modifier: Modifier = Modifier,
+        onClick: () -> Unit,
+        content: @Composable RowScope.() -> Unit
+    ) {
         SideEffect {
-            println("SideEffect")
+            println("Recomposed Button")
         }
         Button(
-            onClick = {
-                currentRecomposeScope.invalidate()
-            }
-        ) {
-            Text(text = "invalidate")
+            modifier = modifier,
+            onClick = onClick,
+            content = content
+        )
+    }
+
+    @Composable
+    private fun LoggingText(
+        modifier: Modifier = Modifier,
+        text: String
+    ) {
+        SideEffect {
+            println("Recomposed Text")
         }
+        Text(
+            modifier = modifier,
+            text = text
+        )
     }
 }
