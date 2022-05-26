4ed738e047257683a1f8a7eb968664170f1aef29
jisungbin <ji@sungb.in>
2022-05-27 04:07:41 +0900

    Test currentRecomposeScope

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -9,16 +9,15 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExplicitGroupsComposable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.currentRecomposeScope
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
 
@@ -36,6 +35,7 @@ class MainActivity : ComponentActivity() {
     private lateinit var binding: ActivityMainBinding
     private val vm: MainViewModel by viewModels()
 
+    @ExplicitGroupsComposable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         /*binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -51,12 +51,13 @@ class MainActivity : ComponentActivity() {
         }*/
 
         setContent {
-            Box(
+            /*Box(
                 modifier = Modifier.fillMaxSize(),
                 contentAlignment = Alignment.Center,
             ) {
                 Test()
-            }
+            }*/
+            println(currentRecomposeScope)
         }
     }
 
