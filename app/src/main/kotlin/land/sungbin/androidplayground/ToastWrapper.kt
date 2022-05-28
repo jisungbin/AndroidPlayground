40095a8a6803c2fe1d85b5a5d3ec8c98ab0c8d90
jisungbin <ji@sungb.in>
2022-05-28 23:34:17 +0900

    Create ToastWrapper for Compose

---
+++ app/src/main/kotlin/land/sungbin/androidplayground/ToastWrapper.kt
@@ -0,0 +1,42 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
+package land.sungbin.androidplayground
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+
+class ToastWrapper(private val context: Context) {
+    private val instance by lazy { Toast(context) }
+
+    operator fun invoke(
+        message: String,
+        length: Int = Toast.LENGTH_SHORT
+    ) {
+        show(
+            message = message,
+            length = length
+        )
+    }
+
+    fun show(
+        message: String,
+        length: Int = Toast.LENGTH_SHORT
+    ) {
+        instance.run {
+            setText(message)
+            duration = length
+            show()
+        }
+    }
+}
+
+@Composable
+fun rememberToast(): ToastWrapper {
+    val context = LocalContext.current.applicationContext
+    return remember(context) {
+        ToastWrapper(context)
+    }
+}
