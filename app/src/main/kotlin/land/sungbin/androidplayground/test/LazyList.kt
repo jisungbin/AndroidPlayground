db4aff7ec08a7a0b3de04f013c0a281c6e9b4536
jisungbin <ji@sungb.in>
2022-05-29 06:00:43 +0900

    Add tests

---
+++ app/src/main/kotlin/land/sungbin/androidplayground/test/LazyList.kt
@@ -0,0 +1,140 @@
+package land.sungbin.androidplayground.test
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
+
+private typealias NativeColor = android.graphics.Color
+
+@Composable
+fun LazyListKeyTest() {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(vertical = 30.dp),
+        verticalArrangement = Arrangement.spacedBy(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        items(
+            items = List(50) { it },
+            key = { number -> number }
+        ) { number ->
+            Text(text = "내 번호: $number")
+        }
+    }
+}
+
+@Composable
+fun ColumnSpacedByTest() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        List(50) { it }.forEach { number ->
+            Text(
+                modifier = Modifier
+                    .border(
+                        width = 1.dp,
+                        color = with(NativeColor.parseColor("#FF9CFF")) {
+                            Color(
+                                red = red,
+                                green = green,
+                                blue = blue
+                            )
+                        }
+                    ),
+                text = "내 번호: $number"
+            )
+        }
+    }
+}
+
+@Composable
+private fun DummyLazyListWithoutKey() {
+
+    LazyColumn {
+        items(List(50) { it }) { number ->
+            Text(
+                modifier = Modifier.padding(vertical = 30.dp),
+                text = "내 번호: $number"
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun DummyLazyListWithKey() {
+
+    LazyColumn {
+        items(
+            items = List(50) { it },
+            key = { number -> number }
+        ) { number ->
+            Text(
+                modifier = Modifier.padding(vertical = 30.dp),
+                text = "내 번호: $number"
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun DummyLazyListSpacedBy() {
+
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(30.dp),
+    ) {
+        items(
+            items = List(50) { it },
+            key = { number -> number }
+        ) { number ->
+            Text(text = "내 번호: $number")
+        }
+    }
+
+}
+
+@Composable
+private fun DummyColumnSpacedBy() {
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(30.dp),
+    ) {
+        repeat(50) { number ->
+            Text(text = "내 번호: $number")
+        }
+    }
+
+}
+
+@Composable
+private fun DummyLazyListContentPadding() {
+
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 30.dp),
+    ) {
+        items(
+            items = List(50) { it },
+            key = { number -> number }
+        ) { number ->
+            Text(text = "내 번호: $number")
+        }
+    }
+
+}
---
+++ app/src/main/kotlin/land/sungbin/androidplayground/test/intrinsic.kt
@@ -0,0 +1,66 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package land.sungbin.androidplayground.test
+
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+private val awesomeTexts = listOf("성빈랜드", "찰스의 안드로이드", "android developer")
+
+@Composable
+fun MenuColumnTest() {
+    Card(
+        modifier = Modifier.width(IntrinsicSize.Min),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp)
+    ) {
+        awesomeTexts.forEach { text ->
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                text = text,
+                style = LocalTextStyle.current.copy(fontSize = 25.sp)
+            )
+        }
+    }
+}
+
+
+@Composable
+private fun DummyMenuColumnWithMax() {
+
+    Card(Modifier.width(IntrinsicSize.Max)) {
+        awesomeTexts.forEach { text ->
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = text
+            )
+        }
+    }
+
+}
+
+@Composable
+private fun DummyMenuColumnWithMin() {
+
+    Card(Modifier.width(IntrinsicSize.Min)) {
+        awesomeTexts.forEach { text ->
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = text
+            )
+        }
+    }
+
+}
