feb4b24a1434b1d81fa5619e8a5b2d2cff0c8261
jisungbin <ji@sungb.in>
2022-05-29 00:25:18 +0900

    Update test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ProvideTextStyle
-import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,6 +21,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
+import land.sungbin.androidplayground.test.ClickableTextTest
 import land.sungbin.androidplayground.theme.DefaultTextStyle
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +58,7 @@ class MainActivity : ComponentActivity() {
                         .padding(PaddingValues(16.dp)),
                     contentAlignment = Alignment.Center,
                 ) {
-                    Text(text = "선택 불가능한 Text")
+                    ClickableTextTest()
                 }
             }
         }
