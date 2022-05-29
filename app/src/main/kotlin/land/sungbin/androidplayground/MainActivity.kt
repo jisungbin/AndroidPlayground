595108f781e60ea79084fe5d0e0ea7ac39a07d79
jisungbin <ji@sungb.in>
2022-05-29 13:51:56 +0900

    Update test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -1,17 +1,15 @@
 @file:Suppress(
-    "SetTextI18n", "unused", "OPT_IN_IS_NOT_ENABLED", "SameParameterValue",
+    "SetTextI18n",
+    "unused",
+    "OPT_IN_IS_NOT_ENABLED",
+    "SameParameterValue",
     "UNCHECKED_CAST"
 )
 
 package land.sungbin.androidplayground
 
 import android.annotation.SuppressLint
-import android.os.Binder
 import android.os.Bundle
-import android.os.Parcelable
-import android.util.Size
-import android.util.SizeF
-import android.util.SparseArray
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -21,9 +19,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ProvideTextStyle
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -31,22 +30,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
-import java.io.Serializable
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
 import land.sungbin.androidplayground.test.BackHandlerTest
 import land.sungbin.androidplayground.theme.DefaultTextStyle
-
-private data class IntHolder(var value: Int)
-
-private val AcceptableClasses = arrayOf(
-    Serializable::class.java,
-    Parcelable::class.java,
-    String::class.java,
-    SparseArray::class.java,
-    Binder::class.java,
-    Size::class.java,
-    SizeF::class.java
-)
 
 class MainActivity : ComponentActivity() {
 
@@ -71,15 +57,18 @@ class MainActivity : ComponentActivity() {
         }*/
 
         setContent {
-            SideEffect {
-                val intHolder = IntHolder(1)
-                AcceptableClasses.forEach { instance ->
-                    println("${instance.name}: ${instance.isInstance(intHolder)}")
+            val intHolder = IntHolder(1)
+            val saver = Saver<IntHolder, List<IntHolder>>(
+                save = { holder ->
+                    listOf(holder)
+                },
+                restore = { holderList ->
+                    holderList.first()
                 }
+            )
+            rememberSaveable(saver = saver) {
+                intHolder
             }
-            /*SideEffect {
-                (arrayListOf(mutableStateOf(IntHolder(1))) as ArrayList<Parcelable>).also(::println)
-            }*/
 
             LaunchedEffect(Unit) {
                 window.setFlags( // 네비게이션바까지 영역 확장하려면 필요
