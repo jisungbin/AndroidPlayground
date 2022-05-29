cbd534b80968b31fb5101be57e3af2554d735c1e
jisungbin <ji@sungb.in>
2022-05-29 13:52:15 +0900

    Add localtest file

---
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -0,0 +1,26 @@
+package land.sungbin.androidplayground
+
+import android.os.Binder
+import android.os.Parcelable
+import android.util.Size
+import android.util.SizeF
+import android.util.SparseArray
+import java.io.Serializable
+
+private val AcceptableClasses = listOf(
+    Serializable::class.java,
+    Parcelable::class.java,
+    String::class.java,
+    SparseArray::class.java,
+    Binder::class.java,
+    Size::class.java,
+    SizeF::class.java
+)
+
+fun main() {
+    val intHolder = listOf(IntHolder(1))
+    AcceptableClasses.forEach { instance ->
+        println("${instance.simpleName}: ${instance.isInstance(intHolder)}")
+    }
+    // println(ArrayList(listOf(1)))
+}
