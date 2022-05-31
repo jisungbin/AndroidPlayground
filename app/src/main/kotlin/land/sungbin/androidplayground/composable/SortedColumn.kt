4e213131535da439a4cbde2eee63fd782cff1d33
jisungbin <ji@sungb.in>
2022-05-31 11:28:00 +0900

    Create SortedColumn function

---
+++ app/src/main/kotlin/land/sungbin/androidplayground/composable/SortedColumn.kt
@@ -0,0 +1,23 @@
+package land.sungbin.androidplayground.composable
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SortedColumn(content: @Composable ColumnScope.() -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(
+            space = 30.dp,
+            alignment = Alignment.CenterVertically
+        ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        content = content
+    )
+}
