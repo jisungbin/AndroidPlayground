8986c57ee2dbaed68a5534fb751a53bac5f83d6f
jisungbin <ji@sungb.in>
2022-06-09 23:11:38 +0900

    Snapshot test

--- app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -1,5 +1,23 @@
 package land.sungbin.androidplayground
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.Snapshot
+
 fun main() {
-    print("Bye world.")
+    var age by mutableStateOf(1)
+    val globalSnapshot = Snapshot.takeMutableSnapshot()
+    val snap1 = Snapshot.takeMutableSnapshot()
+    snap1.enter {
+        age = 20
+        println("A: $age")
+    }
+    println("B: $age")
+    globalSnapshot.enter {
+        println("C: $age")
+        snap1.apply()
+        println("D: $age")
+    }
+    println("E: $age")
 }
