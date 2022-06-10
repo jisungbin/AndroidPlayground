9a4152001d9a78695fbb4c56743b655b0dd2cb17
jisungbin <ji@sungb.in>
2022-06-10 21:25:37 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -1,23 +1,25 @@
+@file:Suppress("RedundantUnitReturnType", "unused")
+
 package land.sungbin.androidplayground
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.Snapshot
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import land.sungbin.androidplayground.test.setContent
 
-fun main() {
-    var age by mutableStateOf(1)
-    val globalSnapshot = Snapshot.takeMutableSnapshot()
-    val snap1 = Snapshot.takeMutableSnapshot()
-    snap1.enter {
-        age = 20
-        println("A: $age")
+fun main(): Unit {
+    val isWorld = true
+    setContent {
+        Column {
+            Row {
+                Text("Hi world!")
+                if (!isWorld) {
+                    Text("Are you world?")
+                }
+            }
+            if (isWorld) {
+                Text("Bye world!")
+            }
+        }
     }
-    println("B: $age")
-    globalSnapshot.enter {
-        println("C: $age")
-        snap1.apply()
-        println("D: $age")
-    }
-    println("E: $age")
 }
