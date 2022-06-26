96aafef60fa312d669d225c3d5987eb1c688ca67
jisungbin <ji@sungb.in>
2022-06-27 02:08:42 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -1,4 +1,8 @@
-@file:Suppress("RedundantUnitReturnType", "unused")
+@file:Suppress(
+    "RedundantUnitReturnType",
+    "unused",
+    "UnusedImport"
+)
 @file:NoLiveLiterals
 
 package land.sungbin.androidplayground
@@ -7,6 +11,7 @@ import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.simulateHotReload
 import androidx.compose.runtime.withRunningRecomposer
+import land.sungbin.androidplayground.test.setContent
 
 fun main() {
     setContent {
