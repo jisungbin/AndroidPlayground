152191eccab2b8bad05af3fad4dcf792dd663803
jisungbin <ji@sungb.in>
2022-06-28 06:52:52 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -7,10 +7,18 @@
 
 package land.sungbin.androidplayground
 
+import androidx.compose.runtime.ControlledComposition
+import androidx.compose.runtime.MovableContentStateReference
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.Recomposer
 import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.simulateHotReload
+import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.runtime.withRunningRecomposer
+import kotlin.coroutines.resume
+import kotlinx.coroutines.CancellableContinuation
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.Job
 import land.sungbin.androidplayground.test.setContent
 
 fun main() {
