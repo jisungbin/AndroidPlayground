199c70b7cf5e6a289b705f9d155ccf8ff1a336ed
jisungbin <ji@sungb.in>
2022-06-29 21:18:24 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -29,95 +29,35 @@ import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDp
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.LocalOverscrollConfiguration
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.paddingFrom
-import androidx.compose.foundation.layout.paddingFromBaseline
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.foundation.text.selection.LocalTextSelectionColors
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.LocalMinimumTouchTargetEnforcement
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Text
-import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material.ripple.LocalRippleTheme
-import androidx.compose.material.Card
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.InternalComposeApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.movableContentOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.layout.FirstBaseline
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalFontFamilyResolver
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.platform.LocalTextToolbar
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 import androidx.databinding.DataBindingUtil
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.roundToInt
-import kotlinx.coroutines.launch
 import land.sungbin.androidplayground.R
 import land.sungbin.androidplayground.composable.SortedColumn
-import land.sungbin.androidplayground.composable.SortedLazyColumn
-import land.sungbin.androidplayground.constant.DisplayFor
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
-import land.sungbin.androidplayground.texttoolbar.SungbinLandTextToolbar
 import land.sungbin.androidplayground.theme.BackgroundWhite
 import land.sungbin.androidplayground.theme.NanumGothicTextStyle
-import land.sungbin.androidplayground.theme.NanumGothic
-import land.sungbin.androidplayground.theme.Pink
-import land.sungbin.androidplayground.theme.PinkOverscrollConfiguration
-import land.sungbin.androidplayground.theme.PinkRippleTheme
-import land.sungbin.androidplayground.theme.PinkTextSelectionColors
 import land.sungbin.androidplayground.viewmodel.MainViewModel
-import land.sungbin.androidplayground.wrapper.rememberToast
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -151,7 +91,36 @@ class MainActivity : ComponentActivity() {
                 )
             }
 
-            ProvideTextStyle(NanumGothicTextStyle) {}
+            ProvideTextStyle(NanumGothicTextStyle) {
+                SortedColumn {
+                    // Create a MutableTransitionState<Boolean> for the AnimatedVisibility.
+                    val visibilityState = remember {
+                        MutableTransitionState(false).apply {
+                            // Start the animation immediately.
+                            targetState = true
+                        }
+                    }
+                    Column {
+                        AnimatedVisibility(visibleState = visibilityState) {
+                            Text(text = "Hello, world!")
+                        }
+
+                        // Use the MutableTransitionState to know the current animation state
+                        // of the AnimatedVisibility.
+                        Text(
+                            modifier = Modifier.clickable {
+                                visibilityState.targetState = !visibilityState.targetState
+                            },
+                            text = when {
+                                visibilityState.isIdle && visibilityState.currentState -> "Visible"
+                                !visibilityState.isIdle && visibilityState.currentState -> "Disappearing"
+                                visibilityState.isIdle && !visibilityState.currentState -> "Invisible"
+                                else -> "Appearing"
+                            }
+                        )
+                    }
+                }
+            }
         }
     }
 }
