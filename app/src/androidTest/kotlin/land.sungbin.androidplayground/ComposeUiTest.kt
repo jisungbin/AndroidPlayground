b1a1fdf519c75c1d8ca293e66fe625e154a5f449
jisungbin <ji@sungb.in>
2022-06-30 22:19:02 +0900

    test

--- app/src/androidTest/kotlin/land.sungbin.androidplayground/ComposeUiTest.kt
+++ app/src/androidTest/kotlin/land.sungbin.androidplayground/ComposeUiTest.kt
@@ -1,9 +1,16 @@
 package land.sungbin.androidplayground
 
 import androidx.compose.material.Text
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -11,12 +18,50 @@ class ComposeUiTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    @Before
+    fun setup() {
+        composeTestRule.mainClock.autoAdvance = false
+    }
+
     @Test
-    fun onCreate() {
+    fun welcome_test() {
         composeTestRule.setContent {
             Text(text = "Welcome")
         }
 
-        composeTestRule.onNodeWithText("Welcome").assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("Welcome")
+            .assertIsDisplayed()
     }
+
+    @Test
+    fun counter_test() {
+        var counter by mutableStateOf(0)
+        var lastSeenValue = 0
+
+        composeTestRule.setContent {
+            Text(counter.toString())
+            SideEffect {
+                lastSeenValue = counter
+            }
+        }
+
+        counter = 1
+
+        lastSeenValue() // 0
+        assertFalse(lastSeenValue == 1)
+        lastSeenValue() // 0
+
+        composeTestRule.onNodeWithText("1").assertDoesNotExist()
+        assertFalse(lastSeenValue == 1) // 0
+
+        composeTestRule.mainClock.advanceTimeByFrame()
+
+        composeTestRule.onNodeWithText("1").assertExists()
+        assertTrue(lastSeenValue == 1) // 1
+    }
+}
+
+private operator fun Any.invoke() {
+    println("TestLog: $this")
 }
