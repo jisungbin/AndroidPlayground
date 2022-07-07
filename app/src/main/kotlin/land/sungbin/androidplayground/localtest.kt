266faa92412edb712fb0eb4890a1c10f7f5cfaa1
jisungbin <ji@sungb.in>
2022-07-08 00:47:55 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/localtest.kt
@@ -7,15 +7,25 @@
 
 package land.sungbin.androidplayground
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.simulateHotReload
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.MeasureScope
 
-fun main() {
-    setContent {
-        movableContentOf {
-            simulateHotReload(1)
-        }
+fun main() = setContent {
+    val animatable = remember {
+        Animatable(Color.Black)
+    }
+    val transition = updateTransition(targetState = Color.Red, label = "")
+
+
+
+    LaunchedEffect(Unit) {
+        animatable.animateTo(Color.Blue)
     }
 }
