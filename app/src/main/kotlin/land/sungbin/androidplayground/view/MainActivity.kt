e3f1add8613b9fba3bcf84b75fa369cd62700896
jisungbin <ji@sungb.in>
2022-07-10 07:22:09 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -27,31 +27,31 @@
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
-import android.view.WindowManager
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.InternalComposeApi
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.text.style.TextAlign
 import androidx.databinding.DataBindingUtil
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import land.sungbin.androidplayground.R
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
-import land.sungbin.androidplayground.snippet.animation.movie.MovieSelectorWithCustomAnimateSpecAndTransitionExtensions
-import land.sungbin.androidplayground.snippet.animation.movie.MovieSelectorWithCustomTabTransition
 import land.sungbin.androidplayground.theme.PlaygroundTheme
 import land.sungbin.androidplayground.viewmodel.MainViewModel
 
@@ -71,7 +71,7 @@ class MainActivity : ComponentActivity() {
         )
 
         setContent {
-            val systemUiController = rememberSystemUiController()
+            /*val systemUiController = rememberSystemUiController()
 
             var visibilityState by remember { mutableStateOf(true) }
             var selectedColorState by remember { mutableStateOf(Color.Red) }
@@ -79,29 +79,62 @@ class MainActivity : ComponentActivity() {
             LaunchedEffect(Unit) {
                 window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
                 WindowCompat.setDecorFitsSystemWindows(window, false)
-                /*systemUiController.setStatusBarColor(
+                *//*systemUiController.setStatusBarColor(
                     color = Color.Transparent,
                     darkIcons = true
                 )
                 systemUiController.setNavigationBarColor(
                     color = Color.White,
                     darkIcons = true
-                )*/
+                )*//*
                 systemUiController.setSystemBarsColor(
                     color = Color.Transparent,
                     darkIcons = true
                 )
-            }
+            }*/
 
             PlaygroundTheme {
-                // MovieSelectorBasic()
-                // MovieSelectorWithHighLevelAnimated()
-                // MovieSelectorWithMovieTabTransition()
-                // TweenPlayground()
-                // MovieSelectorWithCustomAnimateSpec()
-                // MovieSelectorWithCustomAnimateSpecAndTransitionExtensions()
-                MovieSelectorWithCustomTabTransition()
+                Box(modifier = Modifier.fillMaxSize()) {
+                    // MovieSelectorBasic()
+                    // MovieSelectorWithHighLevelAnimated()
+                    // MovieSelectorWithMovieTabTransition()
+                    // TweenPlayground()
+                    // MovieSelectorWithCustomAnimateSpec()
+                    // MovieSelectorWithCustomAnimateSpecAndTransitionExtensions()
+                    // MovieSelectorWithCustomTabTransition()
+                    WrapContentText()
+                    MatchParentText()
+                }
             }
         }
+    }
+
+    @Composable
+    private fun WrapContentText() {
+        SideEffect {
+            println("compoistion WrapContentText")
+        }
+
+        Text(
+            modifier = Modifier
+                .wrapContentSize()
+                .background(color = Color.LightGray),
+            text = "WrapContent"
+        )
+    }
+
+    @Composable
+    private fun MatchParentText() {
+        SideEffect {
+            println("compoistion MatchParentText")
+        }
+
+        Text(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.White),
+            text = "MatchParent",
+            style = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+        )
     }
 }
