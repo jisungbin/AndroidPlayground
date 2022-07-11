bb396045043da82fbcbe59a5e8a0e710ace164d4
jisungbin <ji@sungb.in>
2022-07-11 15:00:14 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.InternalComposeApi
 
 class MainActivity : ComponentActivity() {
+    @OptIn(InternalComposeApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
