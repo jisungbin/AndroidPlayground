49df76cbd6bf18443464f1f43c28110e50df9572
jisungbin <ji@sungb.in>
2022-07-14 19:41:43 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -3,22 +3,42 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.stringResource
-import land.sungbin.androidplayground.R
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text()
+            Column {
+                var number by remember { mutableStateOf(1) }
+                Text(text = "number").also { println("1") }
+                Textt(
+                    modifier = Modifier.clickable { number++ },
+                    text = { number.toString() }
+                ).also { println("2") }
+            }
         }
     }
 }
 
 @Composable
-fun Text() {
-    Text(text = stringResource(R.string.sungbin_land))
+fun Textt(
+    modifier: Modifier = Modifier,
+    text: () -> String
+) {
+    Text(
+        modifier = modifier,
+        text = text()
+    ).also {
+        println("3")
+    }
 }
