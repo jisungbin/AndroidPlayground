12f1d99fa74b98239a3efb10752642039548367b
jisungbin <ji@sungb.in>
2022-07-14 10:17:42 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -4,21 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.ui.res.stringResource
+import land.sungbin.androidplayground.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Text(text = "Text22")
+            Text(text = stringResource(R.string.sungbin_land))
         }
     }
 }
-
-object TextHolder {
-    const val Text = "TEXT"
-}
-
-/* --------------------------------------------------- */
-
-
