ed9930723c5425b7856e127d6169ff0d78b9b129
jisungbin <ji@sungb.in>
2022-07-14 18:04:32 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -33,24 +33,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.InternalComposeApi
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.res.stringResource
 import androidx.databinding.DataBindingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import land.sungbin.androidplayground.R
+import land.sungbin.androidplayground.composable.LoggingText
+import land.sungbin.androidplayground.composable.SortedColumn
 import land.sungbin.androidplayground.databinding.ActivityMainBinding
 import land.sungbin.androidplayground.theme.PlaygroundTheme
 import land.sungbin.androidplayground.viewmodel.MainViewModel
@@ -71,70 +70,17 @@ class MainActivity : ComponentActivity() {
         )
 
         setContent {
-            /*val systemUiController = rememberSystemUiController()
-
-            var visibilityState by remember { mutableStateOf(true) }
-            var selectedColorState by remember { mutableStateOf(Color.Red) }
-
-            LaunchedEffect(Unit) {
-                window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
-                WindowCompat.setDecorFitsSystemWindows(window, false)
-                *//*systemUiController.setStatusBarColor(
-                    color = Color.Transparent,
-                    darkIcons = true
-                )
-                systemUiController.setNavigationBarColor(
-                    color = Color.White,
-                    darkIcons = true
-                )*//*
-                systemUiController.setSystemBarsColor(
-                    color = Color.Transparent,
-                    darkIcons = true
-                )
-            }*/
+            var number by remember { mutableStateOf(1) }
 
             PlaygroundTheme {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    // MovieSelectorBasic()
-                    // MovieSelectorWithHighLevelAnimated()
-                    // MovieSelectorWithMovieTabTransition()
-                    // TweenPlayground()
-                    // MovieSelectorWithCustomAnimateSpec()
-                    // MovieSelectorWithCustomAnimateSpecAndTransitionExtensions()
-                    // MovieSelectorWithCustomTabTransition()
-                    WrapContentText()
-                    MatchParentText()
+                SortedColumn {
+                    LoggingText(text = stringResource(R.string.sungbin_land))
+                    LoggingText(
+                        modifier = Modifier.clickable { number++ },
+                        text = number.toString()
+                    )
                 }
             }
         }
-    }
-
-    @Composable
-    private fun WrapContentText() {
-        SideEffect {
-            println("compoistion WrapContentText")
-        }
-
-        Text(
-            modifier = Modifier
-                .wrapContentSize()
-                .background(color = Color.LightGray),
-            text = "WrapContent"
-        )
-    }
-
-    @Composable
-    private fun MatchParentText() {
-        SideEffect {
-            println("compoistion MatchParentText")
-        }
-
-        Text(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.White),
-            text = "MatchParent",
-            style = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
-        )
     }
 }
