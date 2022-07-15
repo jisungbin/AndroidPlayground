71f8e55458624a6f44fd6fec1b7dfa533eaff60d
jisungbin <ji@sungb.in>
2022-07-15 18:21:14 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -5,27 +5,23 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.res.stringResource
+import land.sungbin.androidplayground.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var number by remember { mutableStateOf(0) }
-            val text = remember(number) { number.toString() }
-            println("setContent recomposition")
-            Button(onClick = { number++ }) {
-                Text(
-                    text = text,
-                ).also { println("Text recomposition") }
-            }.also { println("Button recomposition") }
+            Text(getString())
         }
     }
 }
+
+@ReadOnlyComposable
+@Composable
+fun getString() = stringResource(R.string.sungbin_land)
