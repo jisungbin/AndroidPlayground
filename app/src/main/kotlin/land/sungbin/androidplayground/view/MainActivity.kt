169970d1033e551e4fb4975f4f9eea38ea674da0
jisungbin <ji@sungb.in>
2022-07-15 12:03:32 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -5,27 +5,42 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import land.sungbin.androidplayground.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {
+        /*setContent {
             var number by remember { mutableStateOf(0) }
             println("setContent recomposition")
-
             Button(onClick = { number++ }) {
                 Text(
                     text = number.toString(),
                 ).also { println("Text recomposition") }
             }.also { println("Button recomposition") }
         }
+
+        setContent {
+            var number by remember { mutableStateOf(0) }
+            println("setContent recomposition")
+            Text(
+                modifier = Modifier.clickable { number++ },
+                text = number.toString(),
+            ).also { println("Text recomposition") }
+        }*/
+
+        setContent {
+            TextWrapper(text = stringResource(R.string.sungbin_land))
+        }
     }
+}
+
+@Composable
+fun TextWrapper(text: String) {
+    Text(text = text)
 }
