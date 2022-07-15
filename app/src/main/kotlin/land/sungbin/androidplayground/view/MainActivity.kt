101c9c81f133348e91d6375be55fbf1f9e9e4fc4
jisungbin <ji@sungb.in>
2022-07-15 09:28:34 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:NoLiveLiterals
+
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
@@ -7,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,27 +21,47 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Column {
+            println("0")
+            Column { // inline -> same recomposition scope with setContent
                 var number by remember { mutableStateOf(1) }
+                var number2 by remember { mutableStateOf(1) }
                 Text(text = "number").also { println("1") }
                 Textt(
                     modifier = Modifier.clickable { number++ },
-                    text = { number.toString() }
+                    text = number.toString() // everytime change instance -> recomposition
                 ).also { println("2") }
+                Texttt( // skippable
+                    modifier = Modifier.clickable { number2++ },
+                    text = { number2.toString() } // always same instance, only change invoke value -> skip recomposition
+                ).also { println("3") }
             }
         }
     }
 }
 
+
 @Composable
 fun Textt(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier,
+        text = text
+    ).also {
+        println("Textt: ${text.hashCode()}")
+    }
+}
+
+@Composable
+fun Texttt(
     modifier: Modifier = Modifier,
     text: () -> String
 ) {
     Text(
         modifier = modifier,
-        text = text()
+        text = text().also { println("Texttt value: ${it.hashCode()}") } // only change value(invoke) instance
     ).also {
-        println("3")
+        println("Texttt: ${text.hashCode()}") // not change reference instance
     }
 }
