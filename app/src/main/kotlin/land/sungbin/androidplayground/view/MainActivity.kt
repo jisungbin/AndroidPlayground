32493265df94558d7221ed4f815ee3604205c58b
jisungbin <ji@sungb.in>
2022-07-16 19:39:43 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -5,8 +5,12 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
 
@@ -14,18 +18,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            DisplayText("SungbinLand")
-            DisplayTexts(listOf("SungbinLand"))
+            var number by remember { mutableStateOf(0) }
+            val text = remember(number) { number.toString() }
+
+            Button(onClick = { number++ }) {
+                Text(text = text)
+            }
         }
     }
-}
-
-@Composable
-fun DisplayText(text: String) {
-    Text(text = text)
-}
-
-@Composable
-fun DisplayTexts(text: List<String>) {
-    Text(text = text.joinToString())
 }
