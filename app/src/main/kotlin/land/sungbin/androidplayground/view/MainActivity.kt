9d163147e138c8db90e3b5b7f832a24d59d0d8f4
jisungbin <ji@sungb.in>
2022-07-17 10:43:31 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -1,22 +1,37 @@
-@file:Suppress(
-    "PrivatePropertyName",
-    "PropertyName"
-)
-
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.collectAsState
-import kotlinx.coroutines.flow.MutableSharedFlow
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import land.sungbin.androidplayground.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MutableSharedFlow<String>().collectAsState(initial = null)
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(150.dp)
+                        .clip(RoundedCornerShape(30.dp)),
+                    painter = painterResource(R.drawable.little_zerry),
+                    contentDescription = null
+                )
+            }
         }
     }
 }
