bce7d533c004127d97f9ce57d7c97b847c550c56
jisungbin <ji@sungb.in>
2022-07-22 12:48:52 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -4,52 +4,11 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        xml()
-    }
-
-    fun xml() {
         setContentView(R.layout.activity_main)
-        findViewById<RecyclerView>(R.id.rv_texts).adapter = TextAdapter(count = 5)
-    }
-
-    fun compose() {
-        setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(space = 30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                repeat(times = 5) {
-                    Text(
-                        text = stringResource(R.string.sungbin_land),
-                        style = TextStyle(
-                            fontFamily = FontFamily(Font(R.font.nanumgothic)),
-                            fontSize = 30.sp,
-                            color = Color.Black
-                        )
-                    )
-                }
-            }
-        }
     }
 }
---
+++ app/src/main/kotlin/land/sungbin/androidplayground/RoundImageView.kt
@@ -0,0 +1,56 @@
+package land.sungbin.androidplayground
+
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.Canvas
+import android.graphics.Path
+import android.graphics.RectF
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+
+class RoundImageView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs) {
+    private var clipPath: Path? = null
+    private var imageRadius = 16.dp
+    private var bitmapRect: RectF? = null
+
+    init {
+        clipPath = Path()
+        val typedArray = context.obtainStyledAttributes(
+            attrs,
+            R.styleable.RoundImageView,
+            0,
+            0
+        )
+        imageRadius = typedArray.getDimensionPixelSize(
+            R.styleable.RoundImageView_riv_radius,
+            this.imageRadius
+        )
+        typedArray.recycle()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        bitmapRect = RectF(0f, 0f, w.toFloat(), h.toFloat())
+    }
+
+    override fun draw(canvas: Canvas) {
+        val clipPath = checkNotNull(clipPath) {
+            "clipPath is not initialized"
+        }
+        clipPath.reset()
+        clipPath.addRoundRect(
+            bitmapRect!!,
+            imageRadius.toFloat(),
+            imageRadius.toFloat(),
+            Path.Direction.CW
+        )
+        canvas.clipPath(clipPath)
+        super.draw(canvas)
+    }
+
+    private val Int.dp: Int
+        get() {
+            val scale = Resources.getSystem().displayMetrics.density
+            return (this * scale + 0.5f).toInt()
+        }
+}
