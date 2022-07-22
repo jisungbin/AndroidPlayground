c89f32480afe1fcd22714949f20bdf281c404508
jisungbin <ji@sungb.in>
2022-07-22 15:16:05 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -2,10 +2,31 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+    }
+
+    fun xml() {
+        // class RoundImageView(context: Context, attrs: AttributeSet?) : AppCompatImageView(context, attrs)
         setContentView(R.layout.activity_main)
+    }
+
+    fun compose() {
+        setContent {
+            Image(
+                modifier = Modifier.clip(RoundedCornerShape(percent = 10)),
+                painter = painterResource(R.drawable.little_zerry),
+                contentDescription = "zerry"
+            )
+        }
     }
 }
