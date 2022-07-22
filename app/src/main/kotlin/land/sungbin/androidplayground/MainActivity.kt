2dd129595a182a0984b5e4cc57f75d2df83eb4bb
jisungbin <ji@sungb.in>
2022-07-22 15:05:55 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -1,4 +1,7 @@
-@file:Suppress("MemberVisibilityCanBePrivate")
+@file:Suppress(
+    "MemberVisibilityCanBePrivate",
+    "SetTextI18n"
+)
 
 package land.sungbin.androidplayground
 
@@ -6,20 +9,12 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 
 class MainActivity : ComponentActivity() {
     private var number = 0
@@ -36,31 +31,17 @@ class MainActivity : ComponentActivity() {
         findViewById<Button>(R.id.btn_state).run {
             setOnClickListener {
                 number++
-                updateText()
+                text = "increase number: $number"
             }
-            updateText()
+            text = "increase number: $number"
         }
     }
 
     fun compose() {
         setContent {
             var number by remember { mutableStateOf(0) }
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
-            ) {
-                Button(onClick = { number++ }) {
-                    Text(
-                        text = stringResource(
-                            R.string.button_number_state,
-                            number
-                        ),
-                        style = LocalTextStyle.current.copy(
-                            fontFamily = FontFamily(Font(R.font.nanumgothic))
-                        )
-                    )
-                }
+            Button(onClick = { number++ }) {
+                Text(text = "increase number: $number")
             }
         }
     }
