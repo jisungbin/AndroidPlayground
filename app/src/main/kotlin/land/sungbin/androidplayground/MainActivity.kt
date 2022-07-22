ff4c1cb267e4dd67b737069beda29e2ff046edfd
jisungbin <ji@sungb.in>
2022-07-22 13:01:15 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -1,14 +1,26 @@
-@file:Suppress("MemberVisibilityCanBePrivate")
-
 package land.sungbin.androidplayground
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    private var number = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn_state).run {
+            setOnClickListener {
+                number++
+                updateText()
+            }
+            updateText()
+        }
+    }
+
+    private fun Button.updateText() {
+        text = getString(R.string.button_number_state, number)
     }
 }
