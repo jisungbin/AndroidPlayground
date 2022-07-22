aa5961602b32b4abdeda280015606976f5327c87
jisungbin <ji@sungb.in>
2022-07-22 15:20:48 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/MainActivity.kt
@@ -5,51 +5,31 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         xml()
     }
 
     fun xml() {
         setContentView(R.layout.activity_main)
+        // + animations, list apdater, click listener, ... etc
+        // class TextAdapter(private val count: Int) : RecyclerView.Adapter<TextAdapter.ViewHolder>()
         findViewById<RecyclerView>(R.id.rv_texts).adapter = TextAdapter(count = 5)
     }
 
     fun compose() {
         setContent {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.spacedBy(space = 30.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                repeat(times = 5) {
-                    Text(
-                        text = stringResource(R.string.sungbin_land),
-                        style = TextStyle(
-                            fontFamily = FontFamily(Font(R.font.nanumgothic)),
-                            fontSize = 30.sp,
-                            color = Color.Black
-                        )
-                    )
+            LazyColumn {
+                items(count = 5) {
+                    Text(text = "성빈랜드")
                 }
             }
         }
     }
+
 }
