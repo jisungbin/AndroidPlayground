c7b48744af2713dd1f005c737740e7e9f8a80b8b
jisungbin <ji@sungb.in>
2022-07-24 15:23:55 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -1,12 +1,26 @@
 @file:NoLiveLiterals
 @file:Suppress(
     "UNUSED_PARAMETER",
-    "FunctionName"
+    "FunctionName",
+    "UNUSED_VARIABLE"
 )
 
 package land.sungbin.androidplayground.view
 
 import androidx.activity.ComponentActivity
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.remember
 
 class MainActivity : ComponentActivity()
+
+@Composable
+fun MovableContentOf() {
+    val content = remember {
+        movableContentOf {
+            Text(text = "AA")
+        }
+    }
+}
