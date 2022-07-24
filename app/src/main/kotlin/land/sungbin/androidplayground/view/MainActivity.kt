a61f0f83977401d4a28a26cc7e6ebdafac847489
jisungbin <ji@sungb.in>
2022-07-25 02:22:41 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -8,25 +8,6 @@
 package land.sungbin.androidplayground.view
 
 import androidx.activity.ComponentActivity
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.movableContentOf
-import androidx.compose.runtime.remember
 
-class MainActivity : ComponentActivity() {
-    init {
-        repeat(10) { index ->
-            println("AAA: $index")
-        }
-    }
-}
-
-@Composable
-fun MovableContentOf() {
-    val content = remember {
-        movableContentOf {
-            Text(text = System.currentTimeMillis().toString())
-        }
-    }
-}
+class MainActivity : ComponentActivity()
