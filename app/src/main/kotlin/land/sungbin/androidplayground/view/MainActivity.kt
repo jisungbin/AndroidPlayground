27115d08ebf49a3abe08bc7709070718df898708
jisungbin <ji@sungb.in>
2022-08-05 21:21:05 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -6,13 +6,6 @@
 
 package land.sungbin.androidplayground.view
 
-import android.os.Bundle
 import androidx.activity.ComponentActivity
-import kotlinx.coroutines.Dispatchers
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Dispatchers.Main.immediate
-    }
-}
+class MainActivity : ComponentActivity()
