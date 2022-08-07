5399efc9a38134dd7301b52235de603ba7021182
jisungbin <ji@sungb.in>
2022-08-07 20:49:20 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/playground2.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/playground2.kt
@@ -5,6 +5,7 @@ package land.sungbin.androidplayground.view
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.remember
 
 class UnstableClass {
     val list = emptyList<Any>()
@@ -30,4 +31,19 @@ fun Content() {
 
     LambdaComposable(unstableClass::unit)
     LambdaComposable(stableClass::unit)
+}
+
+class UnstableClassUnit(private val unstableClass: UnstableClass) : Function0<Unit> {
+    override operator fun invoke() {
+        unstableClass.unit()
+    }
+}
+
+@Composable
+fun ContentAfter() {
+    val stableClass = StableClass()
+    val unstableClass = UnstableClass()
+
+    LambdaComposable { UnstableClassUnit(unstableClass).invoke() }
+    LambdaComposable(lambda = remember(stableClass) { { stableClass.unit() } })
 }
