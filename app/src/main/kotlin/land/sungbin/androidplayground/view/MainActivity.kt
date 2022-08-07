86c208d3234f5455d65cdd02f0c2908359f065c4
jisungbin <ji@sungb.in>
2022-08-07 15:14:05 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/MainActivity.kt
@@ -1,5 +1,28 @@
+@file:Suppress("PropertyName", "MemberVisibilityCanBePrivate")
+
 package land.sungbin.androidplayground.view
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 
-class MainActivity : ComponentActivity()
+val EmptyLambdaInFile = {}
+
+class MainActivity : ComponentActivity() {
+    val EmptyLambdaInActivity = {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LambdaComposable(lambda = EmptyLambdaInFile)
+            LambdaComposable(lambda = EmptyLambdaInActivity)
+        }
+    }
+}
+
+@Composable
+fun LambdaComposable(lambda: () -> Unit) {
+    Text(text = lambda.toString())
+}
