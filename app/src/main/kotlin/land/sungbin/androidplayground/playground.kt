bf7a987f20d5cfe68d3e2150ef6f450985e3b6c7
jisungbin <ji@sungb.in>
2022-08-14 16:03:41 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/playground.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/playground.kt
@@ -1,17 +1,33 @@
 package land.sungbin.androidplayground
 
 import androidx.activity.ComponentActivity
-import androidx.paging.PagingConfig
+import androidx.compose.foundation.interaction.InteractionSource
+import androidx.compose.material.ButtonElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.State
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 class EmptyActivity : ComponentActivity()
 
+@Stable
+private class Elevation(
+    private val defaultElevation: Dp = 1.dp,
+    private val pressedElevation: Dp = 1.dp,
+    private val disabledElevation: Dp = 1.dp,
+    private val hoveredElevation: Dp = 1.dp,
+    private val focusedElevation: Dp = 1.dp,
+) : ButtonElevation {
+    @Composable
+    override fun elevation(enabled: Boolean, interactionSource: InteractionSource): State<Dp> {
+        throw NotImplementedError()
+    }
+}
+
 fun main() {
-    PagingConfig(
-        pageSize = 0,
-        prefetchDistance = 0,
-        enablePlaceholders = false,
-        initialLoadSize = 1,
-        maxSize = 2,
-        jumpThreshold = 0
-    )
+    val one = Elevation()
+    val two = Elevation()
+    println(one == two)
+    println(one === two)
 }
