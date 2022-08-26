6cb2209874c98ac978226280f7f019dfb8a81fa7
jisungbin <ji@sungb.in>
2022-08-26 23:21:27 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/playground.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/playground.kt
@@ -1,10 +1,23 @@
 package land.sungbin.androidplayground
 
+import androidx.compose.material.Button
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import land.sungbin.androidplayground.view.Counter
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 
 @Composable
 fun DelegateOtherComposable() {
     Counter()
 }
 
+@Composable
+fun Counter() {
+    var number by remember { mutableStateOf(1) }
+    Button(onClick = { number++ }) {
+        Text(text = number.toString())
+    }
+}
--- app/src/main/kotlin/land/sungbin/androidplayground/view/playground3.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/playground3.kt
@@ -2,18 +2,4 @@
 
 package land.sungbin.androidplayground.view
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
-@Composable
-fun Counter() {
-    var number by remember { mutableStateOf(1) }
-    Button(onClick = { number++ }) {
-        Text(text = number.toString())
-    }
-}
