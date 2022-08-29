0e8bc4d09c04395598c89c46ce1aece4ed4cce49
jisungbin <ji@sungb.in>
2022-08-29 10:52:16 +0900

    wtf

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,17 +1,41 @@
 @file:Suppress("MayBeConstant")
+@file:NoLiveLiterals
 
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import land.sungbin.androidplayground.DelegateOtherComposable
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.mutableStateOf
+
+class Counter {
+    val list = emptyList<Any>()
+    val number = mutableStateOf(1)
+
+    fun increaseNumber() {
+        number.value++
+    }
+}
 
 class MainActivity : ComponentActivity() {
+    private val numberHolder = Counter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DelegateOtherComposable()
+            val onClickLambda = { numberHolder.increaseNumber() }
+            Button(
+                onClick = onClickLambda.also {
+                    println(it.hashCode())
+                }
+            ) {
+                Text(
+                    text = numberHolder.number.value.toString(),
+                ).also { println("Text recomposition") }
+            }.also { println("Button recomposition") }
         }
     }
 }
