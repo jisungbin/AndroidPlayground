578ffbccfe70c385d47b1653f5c9239aad4cb9f3
jisungbin <ji@sungb.in>
2022-09-03 16:42:09 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -6,17 +6,29 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.ui.res.stringResource
-import land.sungbin.androidplayground.R
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onPlaced
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(
-                text = stringResource(R.string.sungbin_land),
+            BasicTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .onPlaced {
+                        println("onPlaced: ${it.size}")
+                    },
+                value = "Hello",
+                onValueChange = { },
+                onTextLayout = {
+                    println("onTextLayout: ${it.size}")
+                },
             )
         }
     }
