bf4a8004502f370e821f5f0e7665e4bb40852bd4
jisungbin <ji@sungb.in>
2022-09-10 21:39:15 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -6,30 +6,39 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.graphics.Color
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicTextField(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .onPlaced {
-                        println("onPlaced: ${it.size}")
-                    },
-                value = "Hello",
-                onValueChange = { },
-                onTextLayout = {
-                    println("onTextLayout: ${it.size}")
-                },
-            )
+            var state by remember { mutableStateOf("AA") }
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                BasicTextField(
+                    modifier = Modifier
+                        .background(
+                            color = Color.Red,
+                        )
+                        .wrapContentSize(),
+                    value = state,
+                    onValueChange = { state = it },
+                )
+            }
         }
     }
 }
