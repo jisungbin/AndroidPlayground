2a2eb582083dcaf6514d2c2930459487ff8a9880
jisungbin <ji@sungb.in>
2022-09-13 21:56:17 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,43 +1,29 @@
 @file:Suppress("MayBeConstant")
 @file:NoLiveLiterals
+@file:OptIn(ExperimentalMaterial3Api::class)
 
 package land.sungbin.androidplayground.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 
 class PlaygroundActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var state by remember { mutableStateOf("AA") }
-            Box(
+            Scaffold(
                 modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
+
             ) {
-                BasicTextField(
-                    modifier = Modifier
-                        .background(
-                            color = Color.Red,
-                        )
-                        .wrapContentSize(),
-                    value = state,
-                    onValueChange = { state = it },
-                )
+
             }
         }
     }
