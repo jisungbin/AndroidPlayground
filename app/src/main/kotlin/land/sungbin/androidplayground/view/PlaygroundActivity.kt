973c0b6e3c0837bbcf1221d7466852df3ef61f51
jisungbin <ji@sungb.in>
2022-09-15 22:16:50 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,30 +1,19 @@
 @file:Suppress("MayBeConstant")
 @file:NoLiveLiterals
-@file:OptIn(ExperimentalMaterial3Api::class)
 
 package land.sungbin.androidplayground.view
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.ui.Modifier
+import team.duckie.quackquack.ui.component.QuackBody1
 
 class PlaygroundActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-
-            ) {
-
-            }
+            QuackBody1(text = "Hello, World!")
         }
     }
 }
