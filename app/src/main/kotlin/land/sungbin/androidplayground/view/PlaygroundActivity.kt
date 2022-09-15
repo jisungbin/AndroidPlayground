8333e2391b4735ca0218e5876d890b74df310596
jisungbin <ji@sungb.in>
2022-09-15 22:31:24 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -6,14 +6,16 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
 import androidx.compose.runtime.NoLiveLiterals
-import team.duckie.quackquack.ui.component.QuackBody1
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            QuackBody1(text = "Hello, World!")
+            Text(
+                text = "Hello, World!",
+            )
         }
     }
 }
