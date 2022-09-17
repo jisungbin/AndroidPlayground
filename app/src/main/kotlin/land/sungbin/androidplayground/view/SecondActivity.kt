e727b3d16426f8ac61dd8c4e220be6ad737b290e
jisungbin <ji@sungb.in>
2022-09-18 00:40:06 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/SecondActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/SecondActivity.kt
@@ -6,19 +6,13 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.NoLiveLiterals
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import land.sungbin.androidplayground.R
 
 class SecondActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CoroutineScope(Dispatchers.Default).launch {
-            println(getDelayedValue(1000, 1))
-        }
     }
 
     private suspend fun <T> getDelayedValue(
