24adeabae96fac8e1499bda5636aa2a0da3c8163
jisungbin <ji@sungb.in>
2022-09-21 18:26:31 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -6,24 +6,12 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.delay
 import land.sungbin.androidplayground.R
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launchWhenCreated {
-            println(getDelayedValue(1000, 1))
-        }
+        getString(R.string.sungbin_land)
     }
-}
-
-private suspend fun <T> getDelayedValue(
-    delay: Long,
-    value: T,
-): T {
-    delay(delay)
-    return value
 }
