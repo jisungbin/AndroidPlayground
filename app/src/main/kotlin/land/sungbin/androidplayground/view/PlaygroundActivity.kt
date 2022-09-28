75c260bb9eb6e3281d6edf786c879b3cc5b58595
jisungbin <ji@sungb.in>
2022-09-28 12:24:22 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -5,56 +5,28 @@ package land.sungbin.androidplayground.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import land.sungbin.androidplayground.R
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            var number by remember {
-                mutableStateOf(
-                    value = 0,
-                )
-            }
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Button(
-                    modifier = Modifier.wrapContentSize(),
-                    onClick = { number = 10000 },
-                ) {
-                    Text(
-                        text = animatedNumber(
-                            number = number,
-                        ).toString(),
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+        println("onCreate")
     }
-}
 
-@Composable
-fun animatedNumber(
-    number: Int,
-): Int {
-    val animatedNumber by animateIntAsState(
-        targetValue = number,
-    )
-    return animatedNumber
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy")
+    }
 }
