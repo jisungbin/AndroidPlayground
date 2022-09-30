a17c25f4f8f8e4f318fd7edc80878bb7627bf19f
jisungbin <ji@sungb.in>
2022-09-30 21:45:01 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -18,6 +18,7 @@ class PlaygroundActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         println("onStart")
+        recreate()
     }
 
     override fun onPause() {
