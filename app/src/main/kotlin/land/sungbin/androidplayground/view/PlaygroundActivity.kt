281e61852928c9e99f828697d453e3e331a4f536
jisungbin <ji@sungb.in>
2022-10-16 15:55:03 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -4,16 +4,11 @@
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.NoLiveLiterals
-import land.sungbin.androidplayground.R
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val text = TextView(this)
-        text.setText(R.string.sungbin_land)
-        setContentView(text)
     }
 }
