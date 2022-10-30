a2aaa877cf700136b3d6ed538baf7157b69c81e3
jisungbin <ji@sungb.in>
2022-10-30 18:34:04 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -6,17 +6,13 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 
 class NumberHolder {
     var number by mutableStateOf(1)
@@ -27,19 +23,11 @@ class PlaygroundActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        println("Creation Activity.")
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .clickable { numberHolder.number++ },
-                contentAlignment = Alignment.Center,
+            Column(
+                modifier = Modifier.fillMaxSize(),
             ) {
-                Text(
-                    text = "Hello, World!" +
-                            "orientation is ${LocalConfiguration.current.orientation}, " +
-                            "number is ${numberHolder.number}",
-                )
+
             }
         }
     }
