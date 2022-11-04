1c681a836bf83d8b4b24e5eb575e4e175a3c658c
jisungbin <ji@sungb.in>
2022-11-04 14:35:18 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -4,74 +4,40 @@
 
 package land.sungbin.androidplayground.view
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.unit.dp
 
 class PlaygroundActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val orientation = LocalConfiguration.current.orientation
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Box(
-                    modifier = Modifier
-                        .size(size = (orientation * 100).dp)
-                        .animateContentSize(
-                            animationSpec = tween(
-                                durationMillis = 1000,
-                                easing = LinearEasing,
-                            ),
-                        )
-                        .rotate(
-                            degrees = animateFloatAsState(
-                                targetValue = when (orientation) {
-                                    Configuration.ORIENTATION_PORTRAIT -> 0f
-                                    Configuration.ORIENTATION_LANDSCAPE -> 90f
-                                    else -> throw IllegalStateException()
-                                },
-                                animationSpec = tween(
-                                    durationMillis = 1000,
-                                    easing = LinearEasing,
-                                ),
-                            ).value,
-                        )
-                        .background(
-                            color = animateColorAsState(
-                                targetValue = when (orientation) {
-                                    Configuration.ORIENTATION_PORTRAIT -> Color.Yellow
-                                    Configuration.ORIENTATION_LANDSCAPE -> Color.Green
-                                    else -> throw IllegalStateException()
-                                },
-                                animationSpec = tween(
-                                    durationMillis = 1000,
-                                    easing = LinearEasing,
-                                ),
-                            ).value.copy(alpha = 0.5f),
-                        )
-                )
-            }
+            var isLoading by remember { mutableStateOf(false) }
+            println("Recompose setContent")
+
+            Column {
+                Text(
+                    modifier = Modifier.clickable {
+                        isLoading = !isLoading
+                    },
+                    text = "Column Data",
+                ).also { println("Recompose first text") }
+                if (isLoading) {
+                    Text(text = "Loading...")
+                }
+            }.also { println("Recompose Column") }
         }
     }
 }
