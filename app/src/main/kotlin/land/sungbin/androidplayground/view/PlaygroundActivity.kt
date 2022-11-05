ac0d307b4f30f5f4f0b0befb5935f8f9700b0e69
jisungbin <ji@sungb.in>
2022-11-05 21:40:59 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,6 +1,9 @@
-@file:Suppress("MayBeConstant", "UNUSED_ANONYMOUS_PARAMETER", "UNUSED_VARIABLE")
+@file:Suppress(
+    "MayBeConstant", "UNUSED_ANONYMOUS_PARAMETER", "UNUSED_VARIABLE",
+    "KotlinRedundantDiagnosticSuppress", "UnnecessaryOptInAnnotation"
+)
 @file:NoLiveLiterals
-@file:OptIn(ExperimentalAnimationApi::class)
+@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
 
 package land.sungbin.androidplayground.view
 
@@ -8,56 +11,85 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.rememberSwipeableState
+import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Box(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxSize(),
                 contentAlignment = Alignment.Center,
             ) {
-                Text(
-                    text = "Hello, World!",
-                    fontSize = 30.sp,
-                )
+                SwipeableSample()
             }
         }
     }
 }
 
 @Composable
-fun Main() {
+fun SwipeableSample() {
     val density = LocalDensity.current
-    val px = with(density) {
-        1.dp.toPx()
-    }
-    val px2 = with(density) {
-        2.dp.toPx()
-    }
-    val px3 = with(density) {
-        3.dp.toPx()
-    }
-    val px4 = with(density) {
-        4.dp.toPx()
+
+    val squareSize = 48.dp
+    val squareSizePx = with(density) { squareSize.toPx() }
+
+    val fullWidht = LocalConfiguration.current.screenWidthDp.dp
+    val fullWidhtPx = with(density) { fullWidht.toPx() }
+
+    val swipeableState = rememberSwipeableState(0)
+    val anchors = mapOf(
+        0f to 0,
+        fullWidhtPx - squareSizePx to 1,
+    )
+
+
+
+    Box(
+        modifier = Modifier
+            .width(fullWidht)
+            .swipeable(
+                state = swipeableState,
+                anchors = anchors,
+                thresholds = { _, _ -> FractionalThreshold(0.3f) },
+                orientation = Orientation.Horizontal
+            )
+            .background(Color.LightGray)
+    ) {
+        Box(
+            Modifier
+                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+                .size(squareSize)
+                .background(Color.DarkGray)
+        )
     }
 }
 
-inline val Dp.px
-    @Composable
-    @ReadOnlyComposable
-    get() = with(LocalDensity.current) {
-        toPx()
-    }
+@Composable
+fun SwipeableTextField(
+    text: String,
+    onTextChange: (String) -> Unit,
+) {
+
+}
