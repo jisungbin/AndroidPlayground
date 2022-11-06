3c51141369951a31d8cd2101b14b55b7fd5b7e43
jisungbin <ji@sungb.in>
2022-11-07 00:02:56 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -11,139 +11,55 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
-import androidx.compose.material.rememberSwipeableState
-import androidx.compose.material.swipeable
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ExplicitGroupsComposable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import kotlin.math.roundToInt
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var text by remember {
-                mutableStateOf("")
-            }
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                SwipeableTextField(
+            CoroutineScopeContent {
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    text = text,
-                    onTextChange = { newText ->
-                        text = newText
-                    },
+                        .fillMaxSize()
+                        .clickable {
+                            launch {
+                                suspend {
+                                    // Do something
+                                }.invoke()
+                            }
+                        },
                 )
             }
         }
     }
 }
 
-enum class TimeState {
-    Future,
-    Now,
+interface CoroutineScopeContent {
+    val coroutineScope: CoroutineScope
+}
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun CoroutineScopeContent.launch(noinline action: suspend CoroutineScope.() -> Unit): Job {
+    return coroutineScope.launch(block = action)
 }
 
 @Composable
-fun SwipeableTextField(
-    modifier: Modifier = Modifier,
-    text: String,
-    onTextChange: (String) -> Unit,
+@ExplicitGroupsComposable
+inline fun CoroutineScopeContent(
+    content: @Composable CoroutineScopeContent.() -> Unit,
 ) {
-    BoxWithConstraints(modifier = modifier) {
-        val density = LocalDensity.current
-
-        val textFieldWidth = 120.dp
-        val textFieldWidthPx = with(density) { textFieldWidth.toPx() }
-
-        val maxWidthPx = with(density) { maxWidth.toPx() }
-
-        val swipeableState = rememberSwipeableState(
-            initialValue = TimeState.Future,
-        )
-        val anchors = remember {
-            mapOf(
-                0f to TimeState.Future,
-                maxWidthPx - textFieldWidthPx to TimeState.Now,
-            )
-        }
-
-        val textFieldRounding = 30.dp
-
-        Box(
-            modifier = Modifier
-                .width(width = maxWidth)
-                .wrapContentHeight()
-                .swipeable(
-                    state = swipeableState,
-                    anchors = anchors,
-                    thresholds = { _, _ -> FractionalThreshold(fraction = 0.3f) },
-                    orientation = Orientation.Horizontal,
-                )
-                .background(color = Color.LightGray)
-        ) {
-            TextField(
-                modifier = Modifier
-                    .width(width = textFieldWidth)
-                    .wrapContentHeight()
-                    .offset {
-                        IntOffset(
-                            x = swipeableState.offset.value.roundToInt(),
-                            y = 0,
-                        )
-                    },
-                value = text,
-                onValueChange = onTextChange,
-                shape = RoundedCornerShape(
-                    topStart = textFieldRounding,
-                    topEnd = textFieldRounding,
-                    bottomStart = animateDpAsState(
-                        targetValue = when (swipeableState.currentValue == TimeState.Future) {
-                            true -> 0.dp
-                            false -> textFieldRounding
-                        }
-                    ).value,
-                    bottomEnd = animateDpAsState(
-                        targetValue = when (swipeableState.currentValue == TimeState.Future) {
-                            true -> textFieldRounding
-                            false -> 0.dp
-                        },
-                    ).value,
-                ),
-                colors = TextFieldDefaults.textFieldColors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                ),
-            )
-        }
-    }
+    object : CoroutineScopeContent {
+        override val coroutineScope = rememberCoroutineScope()
+    }.content()
 }
