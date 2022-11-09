76074c353d83529005ae48acbe4ab6c6ae5e7c59
jisungbin <ji@sungb.in>
2022-11-09 23:01:59 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -11,70 +11,64 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Button
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExplicitGroupsComposable
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            CoroutineScopeContent {
-                Box(
+            val (state, setText) = remember {
+                mutableStateOf(
+                    value = "",
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .systemBarsPadding(),
+                verticalArrangement = Arrangement.spacedBy(
+                    space = 30.dp,
+                    alignment = Alignment.Bottom,
+                ),
+            ) {
+                Button(
                     modifier = Modifier
-                        .fillMaxSize()
-                        .clickable {
-                            launch {
-                                suspend {
-                                    // Do something
-                                }.invoke()
-                            }
-                        },
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    onClick = { /*TODO*/ },
+                ) {
+                    Text(
+                        text = "Button!",
+                    )
+                }
+                TextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .imePadding(),
+                    value = state,
+                    onValueChange = setText,
                 )
             }
         }
     }
-}
-
-@Composable
-fun Screen(vm: AwesomeViewModel) = CoroutineScopeContent {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .clickable {
-                launch {
-                    suspend {
-                        // Do something
-                    }.invoke()
-                }
-            },
-    )
-}
-
-interface CoroutineScopeContent {
-    val coroutineScope: CoroutineScope
-}
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun CoroutineScopeContent.launch(noinline action: suspend CoroutineScope.() -> Unit): Job {
-    return coroutineScope.launch(block = action)
-}
-
-@Composable
-@ExplicitGroupsComposable
-inline fun CoroutineScopeContent(
-    content: @Composable CoroutineScopeContent.() -> Unit,
-) {
-    object : CoroutineScopeContent {
-        override val coroutineScope = rememberCoroutineScope()
-    }.content()
 }
