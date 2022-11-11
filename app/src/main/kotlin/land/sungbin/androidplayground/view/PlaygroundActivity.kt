fce2624378045216481ea7dfb9fe092ec8228b4c
jisungbin <ji@sungb.in>
2022-11-12 01:39:14 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,74 +1,34 @@
-@file:Suppress(
-    "MayBeConstant", "UNUSED_ANONYMOUS_PARAMETER", "UNUSED_VARIABLE",
-    "KotlinRedundantDiagnosticSuppress", "UnnecessaryOptInAnnotation"
-)
 @file:NoLiveLiterals
-@file:OptIn(ExperimentalAnimationApi::class, ExperimentalMaterialApi::class)
 
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Button
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
+
+@Immutable
+class ImmutableClass {
+    val list = listOf(1, 2, 3)
+}
 
 class PlaygroundActivity : ComponentActivity() {
+    val immutableClass = ImmutableClass()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            val (state, setText) = remember {
-                mutableStateOf(
-                    value = "",
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .systemBarsPadding(),
-                verticalArrangement = Arrangement.spacedBy(
-                    space = 30.dp,
-                    alignment = Alignment.Bottom,
-                ),
-            ) {
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight(),
-                    onClick = { /*TODO*/ },
-                ) {
-                    Text(
-                        text = "ㅁㅁ'ㅁㅁ",
-                    )
-                }
-                TextField(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .imePadding(),
-                    value = state,
-                    onValueChange = setText,
-                )
-            }
+            Text(text = numberToString2(1))
         }
     }
 }
+
+@Composable
+fun NumberToString(a: () -> Int) {
+    Text(text = a().toString())
+}
+
+fun numberToString2(a: Int) = a.toString()
