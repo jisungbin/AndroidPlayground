95a8e66c56317136a8ff2a43837a7490e8fe4c59
jisungbin <ji@sungb.in>
2022-11-26 02:40:07 +0900

    ime insets

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -1,37 +1,84 @@
 @file:NoLiveLiterals
+@file:OptIn(ExperimentalLayoutApi::class)
 
 package land.sungbin.androidplayground.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
         setContent {
-            Column {
-                Box(
+            val (text, setText) = remember { mutableStateOf("") }
+            val imeInsets = WindowInsets.ime
+            val navigationBarInsets = WindowInsets.navigationBars
+
+            // println(WindowInsets.ime)
+            // println(WindowInsets.imeAnimationSource)
+            // println(WindowInsets.imeAnimationTarget)
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(WindowInsets.systemBars.asPaddingValues())
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Text(text = "Hello, World!", fontSize = 30.sp)
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .height(100.dp)
-                        .background(color = Color.Blue)
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = Color.Gray)
-                )
+                        .offset {
+                            val imeHeight = imeInsets.getBottom(this)
+                            val nagivationBarHeight = navigationBarInsets.getBottom(this)
+                            // ime height 에 navigation height 가 포함되는 것으로 추측됨
+                            val yOffset = imeHeight.run {
+                                if (this > 0) minus(nagivationBarHeight)
+                                else this
+                            }
+                            IntOffset(x = 0, y = -yOffset)
+                        },
+                    verticalArrangement = Arrangement.spacedBy(15.dp),
+                ) {
+                    Button(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {},
+                    ) {
+                        Text(text = "Button")
+                    }
+                    TextField(
+                        modifier = Modifier.fillMaxWidth(),
+                        value = text,
+                        onValueChange = setText,
+                    )
+                }
             }
         }
     }
