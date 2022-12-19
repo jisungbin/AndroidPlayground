4f2fc02851afa10b7a9bb41c1a2eb16c69a89203
jisungbin <ji@sungb.in>
2022-12-19 23:46:26 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -5,25 +5,12 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.CompositionLocal
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.State
-import androidx.compose.runtime.currentCompositionLocalContext
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            @Suppress("UNCHECKED_CAST")
-            val localContextLocals =
-                currentCompositionLocalContext.javaClass.getDeclaredField("compositionLocals").run {
-                    isAccessible = true
-                    get(currentCompositionLocalContext)
-                } as AbstractMap<CompositionLocal<Any?>, State<Any?>>
-
-            localContextLocals.forEach { (local, state) ->
-                println("local: ${local.toString().substringAfterLast(".")}, state: ${state.value}")
-            }
         }
     }
 }
