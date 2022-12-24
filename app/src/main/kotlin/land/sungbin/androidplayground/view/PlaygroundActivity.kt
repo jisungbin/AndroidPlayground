1cd70d0075f413db31a3efa69dbd169d4d98f4cf
jisungbin <ji@sungb.in>
2022-12-24 23:22:47 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -5,12 +5,29 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
+import androidx.compose.ui.tooling.preview.Preview
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            BasicText(text = "Hello")
         }
     }
+}
+
+@Composable
+@Preview
+private fun Test() {
+    Text(text = "Hello World!")
+}
+
+@Composable
+@Preview
+private fun Test2() {
+    Text(text = "Hello World!2")
 }
