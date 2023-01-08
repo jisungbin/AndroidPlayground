f5d1268964cb28d99158615100cb7a780ad4c35f
jisungbin <ji@sungb.in>
2023-01-08 14:41:53 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -5,20 +5,20 @@ package land.sungbin.androidplayground.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.Text
+import androidx.compose.material.Icon
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.getValue
@@ -27,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,7 +45,7 @@ class PlaygroundActivity : ComponentActivity() {
                     .padding(30.dp),
                 contentAlignment = Alignment.Center,
             ) {
-                CircleStyleTextField(
+                CustomTextField(
                     text = text,
                     onTextChanged = { text = it },
                 )
@@ -52,51 +54,47 @@ class PlaygroundActivity : ComponentActivity() {
     }
 
     @Composable
-    private fun CircleStyleTextField(
+    private fun CustomTextField(
         text: String,
-        onTextChanged: (text: String) -> Unit,
+        onTextChanged: (String) -> Unit,
     ) {
         BasicTextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(60.dp)
-                .border(color = Color.Black, width = 1.dp),
             value = text,
             onValueChange = onTextChanged,
-            decorationBox = {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 8.dp)
-                        .horizontalScroll(rememberScrollState()),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
+            textStyle = LocalTextStyle.current.copy(fontSize = 25.sp),
+            decorationBox = { innerTextField ->
+                Column(
+                    modifier = Modifier.drawWithContent {
+                        drawContent()
+                        drawLine(
+                            color = Color(0xFFEFEFEF),
+                            start = Offset(
+                                x = 0f,
+                                y = size.height - 1.dp.toPx(),
+                            ),
+                            end = Offset(
+                                x = size.width,
+                                y = size.height - 1.dp.toPx(),
+                            ),
+                            strokeWidth = 1.dp.toPx(),
+                        )
+                    },
                 ) {
-                    text.forEach {
-                        CircleText(it)
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(12.dp),
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Default.Search,
+                            contentDescription = null,
+                            tint = Color(0xFFA8A8A8),
+                        )
+                        innerTextField()
                     }
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
             },
         )
-    }
-
-    @Composable
-    private fun CircleText(text: Char) {
-        Box(
-            modifier = Modifier
-                .size(50.dp)
-                .border(
-                    color = Color.Black,
-                    width = 1.dp,
-                    shape = CircleShape,
-                ),
-            contentAlignment = Alignment.Center,
-        ) {
-            Text(
-                text = text.toString(),
-                color = Color.Black,
-                fontSize = 20.sp,
-            )
-        }
     }
 }
