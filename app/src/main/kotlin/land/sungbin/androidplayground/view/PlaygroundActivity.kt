5da887a7c5f508860f4b85aa0907692d9941e2d8
jisungbin <ji@sungb.in>
2023-01-08 13:42:28 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/view/PlaygroundActivity.kt
@@ -15,12 +15,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
 import androidx.compose.runtime.getValue
@@ -29,9 +27,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,34 +43,10 @@ class PlaygroundActivity : ComponentActivity() {
                     .padding(30.dp),
                 contentAlignment = Alignment.Center,
             ) {
-                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
-
-
-                    TextField(
-                        modifier = Modifier
-                            .width(50.dp)
-                            .border(color = Color.Green, width = 1.dp),
-                        value = text,
-                        onValueChange = { text = it },
-                    )
-                    BasicTextField(
-                        modifier = Modifier
-                            .width(50.dp)
-                            .border(color = Color.Green, width = 1.dp),
-                        value = text,
-                        onValueChange = { text = it },
-                    )
-
-
-
-
-
-                }
-
-
-
-
-
+                CircleStyleTextField(
+                    text = text,
+                    onTextChanged = { text = it },
+                )
             }
         }
     }
@@ -86,15 +60,17 @@ class PlaygroundActivity : ComponentActivity() {
             modifier = Modifier
                 .fillMaxWidth()
                 .height(60.dp)
-                .border(color = Color.Green, width = 1.dp),
+                .border(color = Color.Black, width = 1.dp),
             value = text,
             onValueChange = onTextChanged,
             decorationBox = {
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp)
                         .horizontalScroll(rememberScrollState()),
-                    horizontalArrangement = Arrangement.spacedBy(4.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     text.forEach {
                         CircleText(it)
@@ -109,7 +85,6 @@ class PlaygroundActivity : ComponentActivity() {
         Box(
             modifier = Modifier
                 .size(50.dp)
-                .clip(CircleShape)
                 .border(
                     color = Color.Black,
                     width = 1.dp,
@@ -117,7 +92,11 @@ class PlaygroundActivity : ComponentActivity() {
                 ),
             contentAlignment = Alignment.Center,
         ) {
-            Text(text = text.toString(), color = Color.Black)
+            Text(
+                text = text.toString(),
+                color = Color.Black,
+                fontSize = 20.sp,
+            )
         }
     }
 }
