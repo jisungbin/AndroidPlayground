6244c882c76c7032333c8439090c03918f89276b
jisungbin <ji@sungb.in>
2023-01-10 06:41:01 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/SnapshotMutablePair.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/SnapshotMutablePair.kt
@@ -1,7 +1,7 @@
 package land.sungbin.androidplayground
 
 interface SnapshotMutablePair<A, B> {
-    val value: Pair<A, B>
+    var value: Pair<A, B>
     var firstValue: A
     var secondValue: B
 }
--- app/src/main/kotlin/land/sungbin/androidplayground/SnapshotMutablePairImpl.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/SnapshotMutablePairImpl.kt
@@ -3,16 +3,23 @@ package land.sungbin.androidplayground
 import androidx.compose.runtime.snapshots.StateObject
 import androidx.compose.runtime.snapshots.StateRecord
 
-internal class SnapshotMutablePairImpl<A, B> : StateObject, SnapshotMutablePair<A, B> {
+internal class SnapshotMutablePairImpl<A, B>(
+    first: A,
+    second: B,
+) : StateObject, SnapshotMutablePair<A, B> {
     override val firstStateRecord: StateRecord = TODO()
 
     override fun prependStateRecord(value: StateRecord) {
         TODO()
     }
 
-    override val value: Pair<A, B> = TODO()
+    override var value: Pair<A, B> = TODO()
 
-    override var firstValue: A = TODO()
+    override var firstValue: A
+        get() = value.first
+        set(value) = TODO()
 
-    override var secondValue: B = TODO()
+    override var secondValue: B
+        get() = value.second
+        set(value) = TODO()
 }
