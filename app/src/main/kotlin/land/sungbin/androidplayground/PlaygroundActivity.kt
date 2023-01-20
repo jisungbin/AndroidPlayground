c4019862dc1b99ad21244f2a0b5b02ea133916d1
jisungbin <ji@sungb.in>
2023-01-20 19:32:05 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -4,14 +4,16 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.runtime.NoLiveLiterals
+import java.io.File
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {}
+        val filename = "test.txt"
+        val file = File.createTempFile(filename, null, cacheDir)
+        file.writeText("Hello!!!!!")
+        println("read: ${file.readText()}")
+        println("path: ${file.path}")
     }
 }
-
-
