07053e81b6322e9de0ec6f9c8faa3119589ef0e9
jisungbin <ji@sungb.in>
2023-01-20 17:30:46 +0900

    test

---
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -0,0 +1,17 @@
+@file:NoLiveLiterals
+
+package land.sungbin.androidplayground
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.NoLiveLiterals
+
+class PlaygroundActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
+
+
