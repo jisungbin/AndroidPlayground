307b0fa33dbd457535bdb6b1f60d39c2bcc4d403
jisungbin <ji@sungb.in>
2023-01-22 15:05:41 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -4,16 +4,41 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.NoLiveLiterals
-import java.io.File
+import androidx.compose.runtime.NonRestartableComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.layout.Layout
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val filename = "test.txt"
-        val file = File.createTempFile(filename, null, cacheDir)
-        file.writeText("Hello!!!!!")
-        println("read: ${file.readText()}")
-        println("path: ${file.path}")
+        setContent {
+            Test()
+        }
+    }
+}
+
+@Composable
+@NonRestartableComposable
+fun Test() {
+    var value by remember { mutableStateOf(1) }
+    SimpleLayout {
+        Button(onClick = { value++ }) {
+            Text(text = value.toString())
+        }
+    }
+}
+
+@Composable
+fun SimpleLayout(content: @Composable () -> Unit) {
+    Layout(content = content) { _, _ ->
+        layout(0, 0) {}
     }
 }
