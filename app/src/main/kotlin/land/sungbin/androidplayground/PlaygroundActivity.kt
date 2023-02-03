4fddcaf7567e6db7151d0c8cbf13f865b38fbb77
jisungbin <ji@sungb.in>
2023-02-04 07:05:07 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -4,10 +4,13 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
 import androidx.compose.runtime.NoLiveLiterals
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent { Text("HI") }
     }
 }
