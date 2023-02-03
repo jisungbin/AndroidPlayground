1fe17126477eb7cfa8e5a6565a58619d9e3766d6
jisungbin <ji@sungb.in>
2023-02-04 07:04:45 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -5,11 +5,9 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.ui.Modifier
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Modifier
     }
 }
