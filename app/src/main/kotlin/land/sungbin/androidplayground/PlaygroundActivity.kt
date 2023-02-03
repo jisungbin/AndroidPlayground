22f5a995afd4a7422d842f9a5a9b49322ee7b56b
jisungbin <ji@sungb.in>
2023-02-04 06:24:04 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -4,41 +4,12 @@ package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.Text
 import androidx.compose.runtime.NoLiveLiterals
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class PlaygroundActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            var opacity by remember { mutableStateOf(1.0f) }
-            val opacityAnimation by animateFloatAsState(opacity)
-
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Button(onClick = { opacity -= 0.2f }) {
-                    Text(
-                        text = "opacity animation",
-                        style = LocalTextStyle.current.run {
-                            copy(color = color.copy(alpha = opacityAnimation))
-                        },
-                    )
-                }
-            }
-        }
+        Modifier
     }
 }
