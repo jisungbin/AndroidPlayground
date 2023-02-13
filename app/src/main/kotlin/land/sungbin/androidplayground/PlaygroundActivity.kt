fd2d6ed9c237411c0fdd495f4c886bbf2141d8f3
jisungbin <ji@sungb.in>
2023-02-13 19:35:34 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -3,6 +3,21 @@
 package land.sungbin.androidplayground
 
 import androidx.activity.ComponentActivity
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.NoLiveLiterals
 
 class PlaygroundActivity : ComponentActivity()
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Main() {
+    CompositionLocalProvider(
+        // androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
+        LocalMinimumInteractiveComponentEnforcement provides false
+    ) {
+
+    }
+}
