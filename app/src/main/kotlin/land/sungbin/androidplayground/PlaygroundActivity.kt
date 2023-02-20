8e1698b492536f3e0f05520556b4e0768d7da1d5
jisungbin <ji@sungb.in>
2023-02-21 01:11:25 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.runtime.Composable
-import javax.script.ScriptEngineManager
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.NoLiveLiterals
 
