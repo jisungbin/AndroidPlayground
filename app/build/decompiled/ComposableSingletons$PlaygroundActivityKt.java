5381674c4c3852a5de78b5b8d9b455d112470482
jisungbin <ji@sungb.in>
2023-11-13 21:50:35 +0900

    test

---
+++ app/build/decompiled/ComposableSingletons$PlaygroundActivityKt.java
@@ -0,0 +1,27 @@
+package land.sungbin.androidplayground;
+
+import androidx.compose.runtime.internal.ComposableLambdaKt;
+import kotlin.Metadata;
+import kotlin.jvm.functions.Function2;
+import org.jetbrains.annotations.NotNull;
+
+@Metadata(
+   mv = {1, 9, 0},
+   k = 3,
+   xi = 48
+)
+public final class ComposableSingletons$PlaygroundActivityKt {
+   @NotNull
+   public static final ComposableSingletons$PlaygroundActivityKt INSTANCE = new ComposableSingletons$PlaygroundActivityKt();
+   @NotNull
+   public static Function2 lambda-1;
+
+   @NotNull
+   public final Function2 getLambda_1$app_release/* $FF was: getLambda-1$app_release*/() {
+      return lambda-1;
+   }
+
+   static {
+      lambda-1 = (Function2)ComposableLambdaKt.composableLambdaInstance(2023832317, false, null.INSTANCE);
+   }
+}
