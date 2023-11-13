e76e47228eb41d2a30cec554fdbf33db36305283
jisungbin <ji@sungb.in>
2023-11-13 21:44:53 +0900

    test

---
+++ app/build/decompiled/PlaygroundActivityKt.java
@@ -0,0 +1,47 @@
+package land.sungbin.androidplayground;
+
+import androidx.compose.runtime.Composable;
+import androidx.compose.runtime.Composer;
+import androidx.compose.runtime.ComposerKt;
+import androidx.compose.runtime.RecomposeScopeImplKt;
+import androidx.compose.runtime.ScopeUpdateScope;
+import kotlin.Metadata;
+import kotlin.jvm.functions.Function2;
+import org.jetbrains.annotations.Nullable;
+
+@Metadata(
+   mv = {1, 9, 0},
+   k = 2,
+   xi = 48,
+   d1 = {"\u0000\n\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0002\u001a\r\u0010\u0000\u001a\u00020\u0001H\u0007¢\u0006\u0002\u0010\u0002¨\u0006\u0003"},
+   d2 = {"entry", "", "(Landroidx/compose/runtime/Composer;I)V", "app_debug"}
+)
+public final class PlaygroundActivityKt {
+   @Composable
+   public static final void entry(@Nullable Composer $composer, final int $changed) {
+      $composer = $composer.startRestartGroup(520545983);
+      ComposerKt.sourceInformation($composer, "C(entry):PlaygroundActivity.kt#qshby3");
+      if ($changed == 0 && $composer.getSkipping()) {
+         $composer.skipToGroupEnd();
+      } else {
+         if (ComposerKt.isTraceInProgress()) {
+            ComposerKt.traceEventStart(520545983, $changed, -1, "land.sungbin.androidplayground.entry (PlaygroundActivity.kt:190)");
+         }
+
+         System.out.println("Hello, world!");
+         if (ComposerKt.isTraceInProgress()) {
+            ComposerKt.traceEventEnd();
+         }
+      }
+
+      ScopeUpdateScope var10000 = $composer.endRestartGroup();
+      if (var10000 != null) {
+         var10000.updateScope((Function2)(new Function2() {
+            public final void invoke(@Nullable Composer $composer, int $force) {
+               PlaygroundActivityKt.entry($composer, RecomposeScopeImplKt.updateChangedFlags($changed | 1));
+            }
+         }));
+      }
+
+   }
+}
