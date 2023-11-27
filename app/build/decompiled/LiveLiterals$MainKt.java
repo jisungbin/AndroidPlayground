348677a6ae89b856232cc82ddea2bffd4a950def
jisungbin <ji@sungb.in>
2023-11-27 13:58:50 +0900

    test

---
+++ app/build/decompiled/LiveLiterals$MainKt.java
@@ -0,0 +1,132 @@
+package land.sungbin.androidplayground;
+
+import androidx.compose.runtime.State;
+import androidx.compose.runtime.internal.LiveLiteralFileInfo;
+import androidx.compose.runtime.internal.LiveLiteralInfo;
+import androidx.compose.runtime.internal.LiveLiteralKt;
+import kotlin.Metadata;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+@LiveLiteralFileInfo(
+   file = "/Users/jisungbin/AndroidStudioProjects/AndroidPlayground/app/src/main/kotlin/land/sungbin/androidplayground/main.kt"
+)
+@Metadata(
+   mv = {1, 9, 0},
+   k = 3,
+   xi = 48
+)
+public final class LiveLiterals$MainKt {
+   @NotNull
+   public static final LiveLiterals$MainKt INSTANCE = new LiveLiterals$MainKt();
+   private static int Int$0$vararg$arg-0$call-listOf$val-list$fun-main = 1;
+   @Nullable
+   private static State State$Int$0$vararg$arg-0$call-listOf$val-list$fun-main;
+   private static int Int$1$vararg$arg-0$call-listOf$val-list$fun-main = 4;
+   @Nullable
+   private static State State$Int$1$vararg$arg-0$call-listOf$val-list$fun-main;
+   private static int Int$2$vararg$arg-0$call-listOf$val-list$fun-main = 2;
+   @Nullable
+   private static State State$Int$2$vararg$arg-0$call-listOf$val-list$fun-main;
+   private static int Int$3$vararg$arg-0$call-listOf$val-list$fun-main = 6;
+   @Nullable
+   private static State State$Int$3$vararg$arg-0$call-listOf$val-list$fun-main;
+   private static int Int$4$vararg$arg-0$call-listOf$val-list$fun-main = 3;
+   @Nullable
+   private static State State$Int$4$vararg$arg-0$call-listOf$val-list$fun-main;
+
+   @LiveLiteralInfo(
+      key = "Int$0$vararg$arg-0$call-listOf$val-list$fun-main",
+      offset = 73
+   )
+   public final int Int$0$vararg$arg_0$call_listOf$val_list$fun_main/* $FF was: Int$0$vararg$arg-0$call-listOf$val-list$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$0$vararg$arg-0$call-listOf$val-list$fun-main;
+      } else {
+         State var10000 = State$Int$0$vararg$arg-0$call-listOf$val-list$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$0$vararg$arg-0$call-listOf$val-list$fun-main", Int$0$vararg$arg-0$call-listOf$val-list$fun-main);
+            State$Int$0$vararg$arg-0$call-listOf$val-list$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Int$1$vararg$arg-0$call-listOf$val-list$fun-main",
+      offset = 76
+   )
+   public final int Int$1$vararg$arg_0$call_listOf$val_list$fun_main/* $FF was: Int$1$vararg$arg-0$call-listOf$val-list$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$1$vararg$arg-0$call-listOf$val-list$fun-main;
+      } else {
+         State var10000 = State$Int$1$vararg$arg-0$call-listOf$val-list$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$1$vararg$arg-0$call-listOf$val-list$fun-main", Int$1$vararg$arg-0$call-listOf$val-list$fun-main);
+            State$Int$1$vararg$arg-0$call-listOf$val-list$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Int$2$vararg$arg-0$call-listOf$val-list$fun-main",
+      offset = 79
+   )
+   public final int Int$2$vararg$arg_0$call_listOf$val_list$fun_main/* $FF was: Int$2$vararg$arg-0$call-listOf$val-list$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$2$vararg$arg-0$call-listOf$val-list$fun-main;
+      } else {
+         State var10000 = State$Int$2$vararg$arg-0$call-listOf$val-list$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$2$vararg$arg-0$call-listOf$val-list$fun-main", Int$2$vararg$arg-0$call-listOf$val-list$fun-main);
+            State$Int$2$vararg$arg-0$call-listOf$val-list$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Int$3$vararg$arg-0$call-listOf$val-list$fun-main",
+      offset = 82
+   )
+   public final int Int$3$vararg$arg_0$call_listOf$val_list$fun_main/* $FF was: Int$3$vararg$arg-0$call-listOf$val-list$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$3$vararg$arg-0$call-listOf$val-list$fun-main;
+      } else {
+         State var10000 = State$Int$3$vararg$arg-0$call-listOf$val-list$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$3$vararg$arg-0$call-listOf$val-list$fun-main", Int$3$vararg$arg-0$call-listOf$val-list$fun-main);
+            State$Int$3$vararg$arg-0$call-listOf$val-list$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Int$4$vararg$arg-0$call-listOf$val-list$fun-main",
+      offset = 85
+   )
+   public final int Int$4$vararg$arg_0$call_listOf$val_list$fun_main/* $FF was: Int$4$vararg$arg-0$call-listOf$val-list$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$4$vararg$arg-0$call-listOf$val-list$fun-main;
+      } else {
+         State var10000 = State$Int$4$vararg$arg-0$call-listOf$val-list$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$4$vararg$arg-0$call-listOf$val-list$fun-main", Int$4$vararg$arg-0$call-listOf$val-list$fun-main);
+            State$Int$4$vararg$arg-0$call-listOf$val-list$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+}
---
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -0,0 +1,6 @@
+package land.sungbin.androidplayground
+
+fun main() {
+  val list = listOf(1, 4, 2, 6, 3)
+  println(list.sortedBy { it })
+}
