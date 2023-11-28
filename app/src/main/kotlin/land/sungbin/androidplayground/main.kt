128875ae8f1e1146daeb66972efcb7675ce77254
jisungbin <ji@sungb.in>
2023-11-28 13:09:03 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,39 +1,12 @@
 package land.sungbin.androidplayground
 
-// interface AApi
-// class AApiImpl : AApi
-//
-// interface AApi2
-// class AApiImpl2 : AApi2
-//
-// interface AApi3
-// class AApiImpl3 : AApi3
+object NULL
 
-internal object AApi1
-internal object AApi2
+var a: Any = NULL
 
-internal object AApi3 {
-  fun call() = Unit
+fun hasCodeAndMessageLazy(): Int {
+  if (a === NULL) a = 1
+  return a as Int
 }
 
-object PublicApi {
-  fun call() = AApi3.call()
-}
-
-fun main() {
-  PublicApi.call()
-}
-
-object AmsRepository {
-  /** aa */
-  fun apiCall() {
-    println("api call")
-  }
-}
-
-fun usage() {
-  AmsRepository.apiCall()
-}
-
-// AmsRepository.apiCall()
-// DrtDispatcher.repository.call()
+fun hasCodeAndMessageGetter() = 1
