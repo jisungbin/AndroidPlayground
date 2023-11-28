a226eb4289a18c6073ea6da6d309a35b51791669
jisungbin <ji@sungb.in>
2023-11-28 09:05:56 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,6 +1,39 @@
 package land.sungbin.androidplayground
 
-fun main() {
-  val list = listOf(1, 4, 2, 6, 3)
-  println(list.sortedBy { it })
+// interface AApi
+// class AApiImpl : AApi
+//
+// interface AApi2
+// class AApiImpl2 : AApi2
+//
+// interface AApi3
+// class AApiImpl3 : AApi3
+
+internal object AApi1
+internal object AApi2
+
+internal object AApi3 {
+  fun call() = Unit
 }
+
+object PublicApi {
+  fun call() = AApi3.call()
+}
+
+fun main() {
+  PublicApi.call()
+}
+
+object AmsRepository {
+  /** aa */
+  fun apiCall() {
+    println("api call")
+  }
+}
+
+fun usage() {
+  AmsRepository.apiCall()
+}
+
+// AmsRepository.apiCall()
+// DrtDispatcher.repository.call()
