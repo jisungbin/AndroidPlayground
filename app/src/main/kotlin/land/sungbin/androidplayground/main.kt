14a36bc36f6ac32581974c3cd5581e24fc7af073
jisungbin <ji@sungb.in>
2023-12-13 14:06:13 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,7 +1,5 @@
 package land.sungbin.androidplayground
 
 fun main() {
-  var map = mapOf(Any() to 1, Any() to 5, Any() to 3, Any() to 6)
-  map = map.toSortedMap(compareBy { map[it]!! })
-  println(map)
+  println(listOf(1, 2, 3, 4, 5).chunked(2))
 }
