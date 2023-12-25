08b55235f0ffa0a585c9bbe16ab2fda0708453c0
jisungbin <ji@sungb.in>
2023-12-25 19:20:15 +0900

    test

--- app/build/decompiled/LiveLiterals$SecondActivityKt.java
+++ app/build/decompiled/LiveLiterals$SecondActivityKt.java
@@ -20,30 +20,76 @@ public final class LiveLiterals$SecondActivityKt {
    @NotNull
    public static final LiveLiterals$SecondActivityKt INSTANCE = new LiveLiterals$SecondActivityKt();
    @NotNull
-   private static String String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = "SecondActivity: ";
+   private static String String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = "SecondActivity: ";
    @Nullable
-   private static State State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+   private static State State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+   @NotNull
+   private static String String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = "ms";
+   @Nullable
+   private static State State$String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+   private static long Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+   @Nullable
+   private static State State$Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
    private static int Int$class-SecondActivity;
    @Nullable
    private static State State$Int$class-SecondActivity;
 
    @LiveLiteralInfo(
-      key = "String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity",
-      offset = 780
+      key = "String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity",
+      offset = 910
    )
    @NotNull
-   public final String String$0$str$arg_0$call_Text$fun_$anonymous$$arg_3$call_Box$fun_$anonymous$$arg_1$call_setContent$fun_onCreate$class_SecondActivity/* $FF was: String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity*/() {
+   public final String String$0$str$arg_0$call_Text$fun_$anonymous$$arg_9$call_Button$fun_$anonymous$$arg_3$call_Box$fun_$anonymous$$arg_1$call_setContent$fun_onCreate$class_SecondActivity/* $FF was: String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity*/() {
       if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
-         return String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+         return String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
       } else {
-         State var10000 = State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+         State var10000 = State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
          if (var10000 == null) {
-            State var1 = LiveLiteralKt.liveLiteral("String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity", String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity);
-            State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = var1;
+            State var1 = LiveLiteralKt.liveLiteral("String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity", String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity);
+            State$String$0$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = var1;
             var10000 = var1;
          }
 
          return (String)var10000.getValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity",
+      offset = 949
+   )
+   @NotNull
+   public final String String$arg_0$call_getLongExtra$1$str$arg_0$call_Text$fun_$anonymous$$arg_9$call_Button$fun_$anonymous$$arg_3$call_Box$fun_$anonymous$$arg_1$call_setContent$fun_onCreate$class_SecondActivity/* $FF was: String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+      } else {
+         State var10000 = State$String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity", String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity);
+            State$String$arg-0$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = var1;
+            var10000 = var1;
+         }
+
+         return (String)var10000.getValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity",
+      offset = 954
+   )
+   public final long Long$arg_1$call_getLongExtra$1$str$arg_0$call_Text$fun_$anonymous$$arg_9$call_Button$fun_$anonymous$$arg_3$call_Box$fun_$anonymous$$arg_1$call_setContent$fun_onCreate$class_SecondActivity/* $FF was: Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+      } else {
+         State var10000 = State$Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity", Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity);
+            State$Long$arg-1$call-getLongExtra$1$str$arg-0$call-Text$fun-$anonymous$$arg-9$call-Button$fun-$anonymous$$arg-3$call-Box$fun-$anonymous$$arg-1$call-setContent$fun-onCreate$class-SecondActivity = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).longValue();
       }
    }
 
