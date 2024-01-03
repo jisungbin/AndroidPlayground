a6169f4f98869875e680ac681231fec7e2d163ef
jisungbin <ji@sungb.in>
2024-01-03 20:58:54 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,27 +1,11 @@
 package land.sungbin.androidplayground
 
+import androidx.compose.runtime.snapshots.Snapshot
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main(): Unit = runBlocking {
-  val toggle = MutableStateFlow(true)
-
-  launch {
-    toggle.collect { a ->
-      while (a) {
-        println("a")
-        delay(1000)
-        if (!toggle.value) break
-      }
-    }
-  }
-
-  delay(5000)
-  toggle.emit(false)
-  delay(3000)
-  toggle.emit(true)
-  delay(3000)
-  toggle.emit(false)
+fun main() {
+  Snapshot.registerApplyObserver { anies, snapshot ->  }
 }
