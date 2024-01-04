ccc1d519e22f202cc54a784b791f76058b8970ad
jisungbin <ji@sungb.in>
2024-01-04 22:12:25 +0900

    test

--- app/build/decompiled/LiveLiterals$MainKt.java
+++ app/build/decompiled/LiveLiterals$MainKt.java
@@ -19,53 +19,31 @@ import org.jetbrains.annotations.Nullable;
 public final class LiveLiterals$MainKt {
    @NotNull
    public static final LiveLiterals$MainKt INSTANCE = new LiveLiterals$MainKt();
-   @NotNull
-   private static String String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main = "written: ";
+   private static int Int$$this$call-rangeTo$$$this$call-forEach$fun-main = 1;
    @Nullable
-   private static State State$String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main;
-   private static int Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main;
+   private static State State$Int$$this$call-rangeTo$$$this$call-forEach$fun-main;
+   private static int Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main = 10;
    @Nullable
-   private static State State$Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main;
-   @NotNull
-   private static String String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main = "Hello";
+   private static State State$Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main;
+   private static int Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main = 2;
    @Nullable
-   private static State State$String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main;
-   private static int Int$arg-0$call-repeat$fun-main = 10;
+   private static State State$Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
+   private static int Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
    @Nullable
-   private static State State$Int$arg-0$call-repeat$fun-main;
+   private static State State$Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
 
    @LiveLiteralInfo(
-      key = "String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main",
-      offset = 459
+      key = "Int$$this$call-rangeTo$$$this$call-forEach$fun-main",
+      offset = 287
    )
-   @NotNull
-   public final String String$0$str$arg_0$call_println$fun_$anonymous$$arg_0$call_registerGlobalWriteObserver$fun_main/* $FF was: String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main*/() {
+   public final int Int$$this$call_rangeTo$$$this$call_forEach$fun_main/* $FF was: Int$$this$call-rangeTo$$$this$call-forEach$fun-main*/() {
       if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
-         return String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main;
+         return Int$$this$call-rangeTo$$$this$call-forEach$fun-main;
       } else {
-         State var10000 = State$String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main;
+         State var10000 = State$Int$$this$call-rangeTo$$$this$call-forEach$fun-main;
          if (var10000 == null) {
-            State var1 = LiveLiteralKt.liveLiteral("String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main", String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main);
-            State$String$0$str$arg-0$call-println$fun-$anonymous$$arg-0$call-registerGlobalWriteObserver$fun-main = var1;
-            var10000 = var1;
-         }
-
-         return (String)var10000.getValue();
-      }
-   }
-
-   @LiveLiteralInfo(
-      key = "Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main",
-      offset = 532
-   )
-   public final int Int$arg_0$call_mutableIntStateOf$val_intState$delegate$fun_main/* $FF was: Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main*/() {
-      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
-         return Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main;
-      } else {
-         State var10000 = State$Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main;
-         if (var10000 == null) {
-            State var1 = LiveLiteralKt.liveLiteral("Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main", Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main);
-            State$Int$arg-0$call-mutableIntStateOf$val-intState$delegate$fun-main = var1;
+            State var1 = LiveLiteralKt.liveLiteral("Int$$this$call-rangeTo$$$this$call-forEach$fun-main", Int$$this$call-rangeTo$$$this$call-forEach$fun-main);
+            State$Int$$this$call-rangeTo$$$this$call-forEach$fun-main = var1;
             var10000 = var1;
          }
 
@@ -74,37 +52,55 @@ public final class LiveLiterals$MainKt {
    }
 
    @LiveLiteralInfo(
-      key = "String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main",
-      offset = 572
+      key = "Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main",
+      offset = 290
    )
-   @NotNull
-   public final String String$arg_0$call_mutableStateOf$val_stringState$delegate$fun_main/* $FF was: String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main*/() {
+   public final int Int$arg_0$call_rangeTo$$$this$call_forEach$fun_main/* $FF was: Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main*/() {
       if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
-         return String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main;
+         return Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main;
       } else {
-         State var10000 = State$String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main;
+         State var10000 = State$Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main;
          if (var10000 == null) {
-            State var1 = LiveLiteralKt.liveLiteral("String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main", String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main);
-            State$String$arg-0$call-mutableStateOf$val-stringState$delegate$fun-main = var1;
+            State var1 = LiveLiteralKt.liveLiteral("Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main", Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main);
+            State$Int$arg-0$call-rangeTo$$$this$call-forEach$fun-main = var1;
             var10000 = var1;
          }
 
-         return (String)var10000.getValue();
+         return ((Number)var10000.getValue()).intValue();
       }
    }
 
    @LiveLiteralInfo(
-      key = "Int$arg-0$call-repeat$fun-main",
-      offset = 590
+      key = "Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main",
+      offset = 336
    )
-   public final int Int$arg_0$call_repeat$fun_main/* $FF was: Int$arg-0$call-repeat$fun-main*/() {
+   public final int Int$arg_0$call_rem$arg_0$call_EQEQ$cond$if$fun_$anonymous$$arg_0$call_forEach$fun_main/* $FF was: Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main*/() {
       if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
-         return Int$arg-0$call-repeat$fun-main;
+         return Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
       } else {
-         State var10000 = State$Int$arg-0$call-repeat$fun-main;
+         State var10000 = State$Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
          if (var10000 == null) {
-            State var1 = LiveLiteralKt.liveLiteral("Int$arg-0$call-repeat$fun-main", Int$arg-0$call-repeat$fun-main);
-            State$Int$arg-0$call-repeat$fun-main = var1;
+            State var1 = LiveLiteralKt.liveLiteral("Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main", Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main);
+            State$Int$arg-0$call-rem$arg-0$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main = var1;
+            var10000 = var1;
+         }
+
+         return ((Number)var10000.getValue()).intValue();
+      }
+   }
+
+   @LiveLiteralInfo(
+      key = "Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main",
+      offset = 341
+   )
+   public final int Int$arg_1$call_EQEQ$cond$if$fun_$anonymous$$arg_0$call_forEach$fun_main/* $FF was: Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main*/() {
+      if (!LiveLiteralKt.isLiveLiteralsEnabled()) {
+         return Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
+      } else {
+         State var10000 = State$Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main;
+         if (var10000 == null) {
+            State var1 = LiveLiteralKt.liveLiteral("Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main", Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main);
+            State$Int$arg-1$call-EQEQ$cond$if$fun-$anonymous$$arg-0$call-forEach$fun-main = var1;
             var10000 = var1;
          }
 
--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -5,19 +5,10 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.Snapshot
-import androidx.compose.runtime.snapshots.SnapshotMutableState
 
 fun main() {
-  Snapshot.registerGlobalWriteObserver { stateObject ->
-    stateObject as SnapshotMutableState<*>
-    println("written: ${stateObject.value}")
-  }
-
-  var intState by mutableIntStateOf(0)
-  var stringState by mutableStateOf("Hello")
-
-  repeat(10) {
-    intState++
-    stringState += stringState
+  (1..10).forEach loop@{ number ->
+    if (number % 2 == 0) return@loop
+    println(number)
   }
 }
