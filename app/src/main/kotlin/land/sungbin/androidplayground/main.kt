117c76c27a21852d17476eb3d0c1a8087c394e70
jisungbin <ji@sungb.in>
2024-01-05 01:46:15 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,42 +1,10 @@
 package land.sungbin.androidplayground
 
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.snapshots.Snapshot
-import androidx.compose.runtime.snapshots.StateObject
-import androidx.compose.runtime.snapshots.StateRecord
-import androidx.compose.runtime.snapshots.withCurrent
-import androidx.compose.runtime.snapshots.writable
-
 fun main() {
-  val state = mutableIntStateOf(1)
-  val stateObject = state as StateObject
-  val previousRecord = stateObject.copyCurrentRecord()
+  var target = 0
+  val targetGetter = { target }
 
-  ++state.intValue
-
-  val safeZone = Snapshot.takeMutableSnapshot()
-  try {
-    safeZone.enter {
-      val current = state.intValue
-      val previous = run {
-        stateObject.restoreFrom(previousRecord)
-        state.intValue
-      }
-      println("current: $current, previous: $previous")
-    }
-  } finally {
-    safeZone.dispose()
-  }
-
-  println("state: ${state.intValue}")
-}
-
-fun StateObject.copyCurrentRecord(): StateRecord {
-  val newRecord = firstStateRecord.create()
-  firstStateRecord.withCurrent(newRecord::assign)
-  return newRecord
-}
-
-fun StateObject.restoreFrom(record: StateRecord) {
-  firstStateRecord.writable(this) { assign(record) }
+  println(targetGetter())
+  target++
+  println(targetGetter())
 }
