ac66aea6b4ff58bad96d8ea8c5690d99f335f773
jisungbin <ji@sungb.in>
2024-01-11 10:49:21 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/main.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/main.kt
@@ -1,14 +1,3 @@
 package land.sungbin.androidplayground
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.Snapshot
-
-fun main() {
-  (1..10).forEach loop@{ number ->
-    if (number % 2 == 0) return@loop
-    println(number)
-  }
-}
+fun main() {}
