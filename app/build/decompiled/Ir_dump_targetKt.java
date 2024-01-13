bec3a7bb26e68899403c4bf7a0421de50705ab75
jisungbin <ji@sungb.in>
2024-01-13 09:21:03 +0900

    test

--- app/build/decompiled/Ir_dump_targetKt.java
+++ app/build/decompiled/Ir_dump_targetKt.java
@@ -38,16 +38,16 @@ import org.jetbrains.annotations.Nullable;
    d1 = {"\u0000\u0018\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u000e\n\u0002\b\u0002\u001a\u0015\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007¢\u0006\u0002\u0010\u0004\u001a\u0015\u0010\u0005\u001a\u00020\u00012\u0006\u0010\u0006\u001a\u00020\u0007H\u0003¢\u0006\u0002\u0010\b¨\u0006\t"},
    d2 = {"Content", "", "ms", "Landroidx/compose/runtime/MutableLongState;", "(Landroidx/compose/runtime/MutableLongState;Landroidx/compose/runtime/Composer;I)V", "Display", "text", "", "(Ljava/lang/String;Landroidx/compose/runtime/Composer;I)V", "app_debug"}
 )
-@SourceDebugExtension({"SMAP\nPlaygroundActivity.kt\nKotlin\n*S Kotlin\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/PlaygroundActivityKt\n+ 2 Composer.kt\nandroidx/compose/runtime/ComposerKt\n*L\n1#1,235:1\n1097#2,6:236\n*S KotlinDebug\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/PlaygroundActivityKt\n*L\n227#1:236,6\n*E\n"})
-public final class PlaygroundActivityKt {
+@SourceDebugExtension({"SMAP\nir-dump-target.kt\nKotlin\n*S Kotlin\n*F\n+ 1 ir-dump-target.kt\nland/sungbin/androidplayground/Ir_dump_targetKt\n+ 2 Composer.kt\nandroidx/compose/runtime/ComposerKt\n*L\n1#1,61:1\n1097#2,6:62\n*S KotlinDebug\n*F\n+ 1 ir-dump-target.kt\nland/sungbin/androidplayground/Ir_dump_targetKt\n*L\n53#1:62,6\n*E\n"})
+public final class Ir_dump_targetKt {
    @Composable
    @ComposableTarget(
       applier = "androidx.compose.ui.UiComposable"
    )
    public static final void Content(@NotNull final MutableLongState ms, @Nullable Composer $composer, final int $changed) {
       Intrinsics.checkNotNullParameter(ms, "ms");
-      $composer = $composer.startRestartGroup(1419088695);
-      ComposerKt.sourceInformation($composer, "C(Content)226@11024L66,227@11093L39:PlaygroundActivity.kt#qshby3");
+      $composer = $composer.startRestartGroup(456589275);
+      ComposerKt.sourceInformation($composer, "C(Content)52@4199L66,53@4268L39:ir-dump-target.kt#qshby3");
       int $dirty = $changed;
       if (($changed & 14) == 0) {
          $dirty = $changed | ($composer.changed(ms) ? 4 : 2);
@@ -57,10 +57,10 @@ public final class PlaygroundActivityKt {
          $composer.skipToGroupEnd();
       } else {
          if (ComposerKt.isTraceInProgress()) {
-            ComposerKt.traceEventStart(1419088695, $dirty, -1, "land.sungbin.androidplayground.Content (PlaygroundActivity.kt:225)");
+            ComposerKt.traceEventStart(456589275, $dirty, -1, "land.sungbin.androidplayground.Content (ir-dump-target.kt:51)");
          }
 
-         $composer.startReplaceableGroup(-1968071358);
+         $composer.startReplaceableGroup(114597045);
          boolean invalid$iv = $composer.changed(ms);
          int $i$f$cache = false;
          Object it$iv = $composer.rememberedValue();
@@ -81,7 +81,7 @@ public final class PlaygroundActivityKt {
 
          Function0 var4 = (Function0)var10000;
          $composer.endReplaceableGroup();
-         ButtonKt.Button(var4, (Modifier)null, false, (Shape)null, (ButtonColors)null, (ButtonElevation)null, (BorderStroke)null, (PaddingValues)null, (MutableInteractionSource)null, ComposableSingletons$PlaygroundActivityKt.INSTANCE.getLambda-2$app_debug(), $composer, 805306368, 510);
+         ButtonKt.Button(var4, (Modifier)null, false, (Shape)null, (ButtonColors)null, (ButtonElevation)null, (BorderStroke)null, (PaddingValues)null, (MutableInteractionSource)null, ComposableSingletons$Ir_dump_targetKt.INSTANCE.getLambda-1$app_debug(), $composer, 805306368, 510);
          Display(String.valueOf(ms.getLongValue()), $composer, 0);
          if (ComposerKt.isTraceInProgress()) {
             ComposerKt.traceEventEnd();
@@ -92,7 +92,7 @@ public final class PlaygroundActivityKt {
       if (var12 != null) {
          var12.updateScope((Function2)(new Function2() {
             public final void invoke(@Nullable Composer $composer, int $force) {
-               PlaygroundActivityKt.Content(ms, $composer, RecomposeScopeImplKt.updateChangedFlags($changed | 1));
+               Ir_dump_targetKt.Content(ms, $composer, RecomposeScopeImplKt.updateChangedFlags($changed | 1));
             }
          }));
       }
@@ -104,8 +104,8 @@ public final class PlaygroundActivityKt {
       applier = "androidx.compose.ui.UiComposable"
    )
    private static final void Display(final String text, Composer $composer, final int $changed) {
-      $composer = $composer.startRestartGroup(1155949470);
-      ComposerKt.sourceInformation($composer, "C(Display)232@11186L17:PlaygroundActivity.kt#qshby3");
+      $composer = $composer.startRestartGroup(-371844158);
+      ComposerKt.sourceInformation($composer, "C(Display)58@4361L17:ir-dump-target.kt#qshby3");
       int $dirty = $changed;
       if (($changed & 14) == 0) {
          $dirty = $changed | ($composer.changed(text) ? 4 : 2);
@@ -115,7 +115,7 @@ public final class PlaygroundActivityKt {
          $composer.skipToGroupEnd();
       } else {
          if (ComposerKt.isTraceInProgress()) {
-            ComposerKt.traceEventStart(1155949470, $dirty, -1, "land.sungbin.androidplayground.Display (PlaygroundActivity.kt:231)");
+            ComposerKt.traceEventStart(-371844158, $dirty, -1, "land.sungbin.androidplayground.Display (ir-dump-target.kt:57)");
          }
 
          TextKt.Text--4IGK_g(text, (Modifier)null, 0L, 0L, (FontStyle)null, (FontWeight)null, (FontFamily)null, 0L, (TextDecoration)null, (TextAlign)null, 0L, 0, false, 0, 0, (Function1)null, (TextStyle)null, $composer, 14 & $dirty, 0, 131070);
@@ -128,7 +128,7 @@ public final class PlaygroundActivityKt {
       if (var10000 != null) {
          var10000.updateScope((Function2)(new Function2() {
             public final void invoke(@Nullable Composer $composer, int $force) {
-               PlaygroundActivityKt.Display(text, $composer, RecomposeScopeImplKt.updateChangedFlags($changed | 1));
+               Ir_dump_targetKt.Display(text, $composer, RecomposeScopeImplKt.updateChangedFlags($changed | 1));
             }
          }));
       }
---
+++ app/src/main/kotlin/land/sungbin/androidplayground/ir-dump-target.kt
@@ -0,0 +1,60 @@
+package land.sungbin.androidplayground
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableLongState
+
+/*
+{ // BLOCK
+  { // BLOCK
+    val tmp0_safe_receiver: ScopeUpdateScope? = $composer.endRestartGroup()
+    when {
+      EQEQ(arg0 = tmp0_safe_receiver, arg1 = null) -> null
+      else -> tmp0_safe_receiver.updateScope(block = local fun <anonymous>($composer: Composer?, $force: Int) {
+        return Content(ms = ms, $composer = $composer, $changed = updateChangedFlags(flags = $changed.or(other = 1)))
+      })
+    }
+  }
+}
+
+BLOCK type=kotlin.Unit origin=null
+  BLOCK type=kotlin.Unit origin=SAFE_CALL
+    VAR IR_TEMPORARY_VARIABLE name:tmp0_safe_receiver type:androidx.compose.runtime.ScopeUpdateScope? [val]
+      CALL 'public abstract fun endRestartGroup (): androidx.compose.runtime.ScopeUpdateScope? declared in androidx.compose.runtime.Composer' type=androidx.compose.runtime.ScopeUpdateScope? origin=null
+        $this: GET_VAR '$composer: androidx.compose.runtime.Composer? [assignable] declared in land.sungbin.androidplayground.Content' type=androidx.compose.runtime.Composer? origin=null
+    WHEN type=kotlin.Unit origin=IF
+      BRANCH
+        if: CALL 'public final fun EQEQ (arg0: kotlin.Any?, arg1: kotlin.Any?): kotlin.Boolean declared in kotlin.internal.ir' type=kotlin.Boolean origin=null
+          arg0: GET_VAR 'val tmp0_safe_receiver: androidx.compose.runtime.ScopeUpdateScope? [val] declared in land.sungbin.androidplayground.Content' type=androidx.compose.runtime.ScopeUpdateScope? origin=null
+          arg1: CONST Null type=kotlin.Any? value=null
+        then: CONST Null type=kotlin.Any? value=null
+      BRANCH
+        if: CONST Boolean type=kotlin.Boolean value=true
+        then: CALL 'public abstract fun updateScope (block: kotlin.Function2<androidx.compose.runtime.Composer, kotlin.Int, kotlin.Unit>): kotlin.Unit declared in androidx.compose.runtime.ScopeUpdateScope' type=kotlin.Unit origin=null
+          $this: GET_VAR 'val tmp0_safe_receiver: androidx.compose.runtime.ScopeUpdateScope? [val] declared in land.sungbin.androidplayground.Content' type=androidx.compose.runtime.ScopeUpdateScope? origin=null
+          block: FUN_EXPR type=kotlin.Function2<androidx.compose.runtime.Composer?, kotlin.Int, kotlin.Unit> origin=LAMBDA
+            FUN LOCAL_FUNCTION_FOR_LAMBDA name:<anonymous> visibility:local modality:FINAL <> ($composer:androidx.compose.runtime.Composer?, $force:kotlin.Int) returnType:kotlin.Unit
+              VALUE_PARAMETER name:$composer index:0 type:androidx.compose.runtime.Composer?
+              VALUE_PARAMETER name:$force index:1 type:kotlin.Int
+              BLOCK_BODY
+                RETURN type=kotlin.Nothing from='local final fun <anonymous> ($composer: androidx.compose.runtime.Composer?, $force: kotlin.Int): kotlin.Unit declared in land.sungbin.androidplayground.Content'
+                  CALL 'public final fun Content (ms: androidx.compose.runtime.MutableLongState, $composer: androidx.compose.runtime.Composer?, $changed: kotlin.Int): kotlin.Unit declared in land.sungbin.androidplayground' type=kotlin.Unit origin=null
+                    ms: GET_VAR 'ms: androidx.compose.runtime.MutableLongState declared in land.sungbin.androidplayground.Content' type=androidx.compose.runtime.MutableLongState origin=null
+                    $composer: GET_VAR '$composer: androidx.compose.runtime.Composer? declared in land.sungbin.androidplayground.Content.<anonymous>' type=androidx.compose.runtime.Composer? origin=null
+                    $changed: CALL 'internal final fun updateChangedFlags (flags: kotlin.Int): kotlin.Int declared in androidx.compose.runtime.RecomposeScopeImplKt' type=kotlin.Int origin=null
+                      flags: CALL 'public final fun or (other: kotlin.Int): kotlin.Int [infix] declared in kotlin.Int' type=kotlin.Int origin=null
+                        $this: GET_VAR '$changed: kotlin.Int declared in land.sungbin.androidplayground.Content' type=kotlin.Int origin=null
+                        other: CONST Int type=kotlin.Int value=1
+ */
+
+@Composable
+fun Content(ms: MutableLongState) {
+  Button(onClick = { ms.longValue = System.currentTimeMillis() }) {}
+  Display(text = ms.longValue.toString())
+}
+
+@Composable
+private fun Display(text: String) {
+  Text(text = text)
+}
