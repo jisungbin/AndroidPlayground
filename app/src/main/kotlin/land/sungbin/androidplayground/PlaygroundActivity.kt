6441b281b8694d3da173261bed29f250d3df4cae
jisungbin <ji@sungb.in>
2024-06-21 10:16:28 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -5,50 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Row(
-        Modifier
-          .pointerInput(Unit) { println("pointer: $size") }
-          .fillMaxHeight(),
+      Box(
+        modifier = Modifier
+          .fillMaxWidth()
+          .height(300.dp)
+          .verticalScroll(rememberScrollState()),
       ) {
-        // The center of the magenta Box and the baselines of the two texts will be
-        // vertically aligned. Note that alignBy() or alignByBaseline() has to be specified
-        // for all children we want to take part in the alignment. For example, alignByBaseline()
-        // means that the baseline of the text should be aligned with the alignment line
-        // (possibly another baseline) specified for siblings using alignBy or alignByBaseline.
-        // If no other sibling had alignBy() or alignByBaseline(), the modifier would have no
-        // effect.
         Box(
-          modifier = Modifier
-            .size(80.dp, 40.dp)
-            .alignBy { it.measuredHeight / 2 }
-            .background(Color.Magenta)
-        )
-        Text(
-          text = "Text 1",
-          fontSize = 40.sp,
-          modifier = Modifier
-            .alignByBaseline()
-            .background(color = Color.Red)
-        )
-        Text(
-          text = "Text 2",
-          modifier = Modifier
-            .alignByBaseline()
-            .background(color = Color.Cyan)
+          Modifier
+            .fillMaxWidth()
+            .height(600.dp)
+            .background(brush = Brush.linearGradient(listOf(Color(0xFF00FF00), Color(0xFF0000FF))))
+            .drawWithContent {
+              drawContent()
+              drawRect(
+                color = Color.Cyan,
+                size = Size(width = size.width, height = 50.dp.toPx()),
+                topLeft = Offset(x = 0f, y = 550.dp.toPx()),
+              )
+            },
         )
       }
     }
