b56efd3b1e0164aafdddafede23a0a4a2606c779
Ji Sungbin <forky.ji@healingpaper.com>
2024-10-08 13:33:27 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -1,40 +1,156 @@
+@file:OptIn(ExperimentalAnimatableApi::class)
+
 package land.sungbin.androidplayground
 
-import android.app.Notification
 import android.os.Bundle
-import android.service.notification.StatusBarNotification
-import android.widget.RemoteViews
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.DeferredTargetAnimation
+import androidx.compose.animation.core.ExperimentalAnimatableApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LookaheadScope
+import androidx.compose.ui.layout.approachLayout
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.round
 
 class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    println("::StatusBarNotification::")
-    StatusBarNotification::class.java.run {
-      declaredMethods.forEach { method -> println("M[${method.name}] ${method.returnType.name}") }
-      declaredFields.forEach { field -> println("F[${field.name}] ${field.type.name}") }
-    }
+    setContent {
+      LookaheadScope {
+        var isColumn by remember { mutableStateOf(true) }
+        val surfaces = remember {
+          movableContentOf {
+            Box(
+              Modifier
+                .animating(lookaheadScope = this)
+                .surfacing(color = Color.Yellow, isColumn = isColumn),
+            )
+            Box(
+              Modifier
+                .animating(lookaheadScope = this)
+                .surfacing(color = Color.Green, isColumn = isColumn),
+            )
+            Box(
+              Modifier
+                .animating(lookaheadScope = this)
+                .surfacing(color = Color.Blue, isColumn = isColumn),
+            )
+          }
+        }
 
-    println()
-
-    println("::Notification::")
-    Notification::class.java.run {
-      declaredMethods.forEach { method -> println("M[${method.name}] ${method.returnType.name}") }
-      declaredFields.forEach { field -> println("F[${field.name}] ${field.type.name}") }
-    }
-
-    println()
-
-    println("::Notification.Action::")
-    Notification.Action::class.java.run {
-      declaredMethods.forEach { method -> println("M[${method.name}] ${method.returnType.name}") }
-      declaredFields.forEach { field -> println("F[${field.name}] ${field.type.name}") }
-    }
-
-    println("::RemoteViews::")
-    RemoteViews::class.java.run {
-      declaredMethods.forEach { method -> println("M[${method.name}] ${method.returnType.name}") }
-      declaredFields.forEach { field -> println("F[${field.name}] ${field.type.name}") }
+        if (isColumn) {
+          Column(
+            modifier = Modifier
+              .fillMaxSize()
+              .clickable { isColumn = !isColumn },
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally,
+          ) {
+            surfaces()
+          }
+        } else {
+          Row(
+            modifier = Modifier
+              .fillMaxSize()
+              .clickable { isColumn = !isColumn },
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+          ) {
+            surfaces()
+          }
+        }
+      }
     }
   }
 }
+
+private fun Modifier.animating(lookaheadScope: LookaheadScope): Modifier = composed {
+  val coroutineScope = rememberCoroutineScope()
+  val sizeAnimation = remember { DeferredTargetAnimation(IntSize.VectorConverter) }
+  val offsetAnimation = remember { DeferredTargetAnimation(IntOffset.VectorConverter) }
+
+  approachLayout(
+    isMeasurementApproachInProgress = { lookaheadSize ->
+      sizeAnimation.updateTarget(
+        lookaheadSize,
+        coroutineScope = coroutineScope,
+        animationSpec = spring(stiffness = Spring.StiffnessVeryLow)
+      )
+      !sizeAnimation.isIdle
+    },
+    isPlacementApproachInProgress = { lookaheadCoordinates ->
+      val target = with(lookaheadScope) {
+        lookaheadScopeCoordinates.localLookaheadPositionOf(lookaheadCoordinates)
+      }
+      offsetAnimation.updateTarget(
+        target.round(),
+        coroutineScope = coroutineScope,
+        animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+      )
+      !offsetAnimation.isIdle
+    },
+  ) { measurable, constraints ->
+    val (targetWidth, targetHeight) = sizeAnimation.updateTarget(
+      lookaheadSize,
+      coroutineScope = coroutineScope,
+      animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+    )
+    val animatedConstraints = Constraints.fixed(targetWidth, targetHeight)
+    val placeable = measurable.measure(animatedConstraints)
+
+    layout(placeable.width, placeable.height) {
+      val coordinates = coordinates
+      if (coordinates != null) {
+        val target = with(lookaheadScope) {
+          lookaheadScopeCoordinates.localLookaheadPositionOf(coordinates)
+        }
+        val animatedOffset = offsetAnimation.updateTarget(
+          target.round(),
+          coroutineScope = coroutineScope,
+          animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+        )
+        val placementOffset = with(lookaheadScope) {
+          lookaheadScopeCoordinates.localPositionOf(coordinates, relativeToSource = Offset.Zero)
+        }
+
+        val (x, y) = animatedOffset - placementOffset.round()
+        placeable.place(x, y)
+      } else {
+        placeable.place(0, 0)
+      }
+    }
+  }
+}
+
+private fun Modifier.surfacing(color: Color, isColumn: Boolean): Modifier =
+  this
+    .clip(RoundedCornerShape(percent = 15))
+    .background(color = color)
+    .size(size = if (isColumn) 100.dp else 50.dp)
