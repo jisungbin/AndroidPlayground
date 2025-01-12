6903c368563f20dfafa9335b2f61ce5aa350f5e6
Ji Sungbin <ji@sungb.in>
2025-01-13 08:09:21 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -5,33 +5,80 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import cell.component.CellBadgeColor
+import cell.component.CellLargeBadge
+import cell.component.CellMediumBadge
+import cell.style.cellIcon
 
 class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column(Modifier.wrapContentSize()) {
-        Box(
-          Modifier
-            .size(200.dp)
-            .layout { measurable, constraints ->
-              println("$constraints")
-              val placeable = measurable.measure(constraints)
-              layout(placeable.width, placeable.height) {
-                placeable.place(0, 0)
-              }
-            }
-            .drawBehind { println("$size") },
-        )
+      var iconPainter by remember { mutableStateOf<Painter?>(null) }
+
+      Column(modifier = Modifier.fillMaxSize()) {
+        Row(
+          modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+          verticalAlignment = Alignment.CenterVertically,
+          horizontalArrangement = Arrangement.SpaceEvenly,
+        ) {
+          CellLargeBadge(
+            "LargeBadge",
+            CellBadgeColor.Orange,
+            modifier = Modifier.run {
+              val icon = iconPainter
+              if (icon != null) cellIcon(icon, iconDescription = null) else this
+            },
+          )
+
+          CellMediumBadge(
+            "MediumBadge",
+            CellBadgeColor.Orange,
+            modifier = Modifier.run {
+              val icon = iconPainter
+              if (icon != null) cellIcon(icon, iconDescription = null) else this
+            },
+          )
+        }
+        Column(
+          modifier = Modifier
+            .weight(2f)
+            .fillMaxSize(),
+          verticalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterVertically),
+          horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+          val heartPainter = painterResource(R.drawable.ic_round_favorite_24)
+          val robotPainter = painterResource(R.drawable.ic_round_android_24)
+
+          Button(onClick = { iconPainter = null }) {
+            Text("Remove icon")
+          }
+          Button(onClick = { iconPainter = heartPainter }) {
+            Text("Add Heart icon")
+          }
+          Button(onClick = { iconPainter = robotPainter }) {
+            Text("Add Robot icon")
+          }
+        }
       }
     }
   }
