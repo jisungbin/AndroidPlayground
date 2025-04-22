6b412c40c4a047162aa0e28efbe832e20e48e627
Ji Sungbin <ji@sungb.in>
2025-04-22 18:43:18 +0900

    invokeComposable!!!

--- app/build/decompiled/PlaygroundActivity.java
+++ app/build/decompiled/PlaygroundActivity.java
@@ -3,26 +3,63 @@ package land.sungbin.androidplayground;
 import android.os.Bundle;
 import androidx.activity.ComponentActivity;
 import androidx.activity.compose.ComponentActivityKt;
+import androidx.compose.foundation.BackgroundKt;
+import androidx.compose.foundation.layout.SizeKt;
+import androidx.compose.runtime.Composable;
+import androidx.compose.runtime.Composer;
+import androidx.compose.runtime.ComposerKt;
 import androidx.compose.runtime.CompositionContext;
+import androidx.compose.runtime.internal.ComposableLambdaKt;
 import androidx.compose.runtime.internal.StabilityInferred;
+import androidx.compose.ui.Alignment;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.graphics.Color;
+import androidx.compose.ui.graphics.Shape;
 import kotlin.Metadata;
+import kotlin.jvm.functions.Function2;
+import kotlin.jvm.functions.Function3;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Metadata(
    mv = {2, 1, 0},
    k = 1,
    xi = 48,
-   d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u0007¢\u0006\u0004\b\u0002\u0010\u0003J\u0012\u0010\u0004\u001a\u00020\u00052\b\u0010\u0006\u001a\u0004\u0018\u00010\u0007H\u0014¨\u0006\b"},
-   d2 = {"Lland/sungbin/androidplayground/PlaygroundActivity;", "Landroidx/activity/ComponentActivity;", "<init>", "()V", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"}
+   d1 = {"\u0000 \n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0007\u0018\u00002\u00020\u0001B\u0007¢\u0006\u0004\b\u0002\u0010\u0003J\u0012\u0010\u0007\u001a\u00020\b2\b\u0010\t\u001a\u0004\u0018\u00010\nH\u0014R\u0010\u0010\u0004\u001a\u00020\u0005X\u0082\u0004¢\u0006\u0004\n\u0002\u0010\u0006¨\u0006\u000b"},
+   d2 = {"Lland/sungbin/androidplayground/PlaygroundActivity;", "Landroidx/activity/ComponentActivity;", "<init>", "()V", "test", "Lland/sungbin/androidplayground/ComposableClass;", "Lkotlin/jvm/functions/Function3;", "onCreate", "", "savedInstanceState", "Landroid/os/Bundle;", "app_debug"}
 )
 @StabilityInferred(
    parameters = 0
 )
 public final class PlaygroundActivity extends ComponentActivity {
+   @NotNull
+   private final Function3 test;
    public static final int $stable = 8;
+
+   public PlaygroundActivity() {
+      this.test = ComposableClass.constructor-impl(ComposableSingletons$PlaygroundActivityKt.INSTANCE.getLambda$789504108$app_debug());
+   }
 
    protected void onCreate(@Nullable Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
-      ComponentActivityKt.setContent$default((ComponentActivity)this, (CompositionContext)null, ComposableSingletons$PlaygroundActivityKt.INSTANCE.getLambda$2023832317$app_debug(), 1, (Object)null);
+      ComponentActivityKt.setContent$default((ComponentActivity)this, (CompositionContext)null, (Function2)ComposableLambdaKt.composableLambdaInstance(2023832317, true, new Function2() {
+         @Composable
+         public final void invoke(Composer $composer, int $changed) {
+            ComposerKt.sourceInformation($composer, "C29@1110L118:PlaygroundActivity.kt#qshby3");
+            if (($changed & 3) == 2 && $composer.getSkipping()) {
+               $composer.skipToGroupEnd();
+            } else {
+               if (ComposerKt.isTraceInProgress()) {
+                  ComposerKt.traceEventStart(2023832317, $changed, -1, "land.sungbin.androidplayground.PlaygroundActivity.onCreate.<anonymous> (PlaygroundActivity.kt:29)");
+               }
+
+               ComposableClass.invoke-impl(PlaygroundActivity.this.test, BackgroundKt.background-bw27NRU$default(SizeKt.wrapContentSize$default(SizeKt.fillMaxSize$default((Modifier)Modifier.Companion, 0.0F, 1, (Object)null), (Alignment)null, false, 3, (Object)null), Color.Companion.getCyan-0d7_KjU(), (Shape)null, 2, (Object)null), $composer, 6);
+               if (ComposerKt.isTraceInProgress()) {
+                  ComposerKt.traceEventEnd();
+               }
+            }
+
+         }
+      }), 1, (Object)null);
    }
 }
--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -6,40 +6,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.ExperimentalAnimatableApi
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Composer
 import androidx.compose.runtime.InternalComposeApi
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.currentComposer
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Density
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@JvmInline
+value class ComposableClass(
+  val composable: @Composable (Modifier) -> Unit,
+) : @Composable (Modifier) -> Unit by composable
 
 class PlaygroundActivity : ComponentActivity() {
+  private val test = ComposableClass { Text("TTTT", it) }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      val composer = currentComposer
-
-      LaunchedEffect(composer) {
-        println("CALL!!")
-        println(
-          invokeComposable<Painter>(composer) {
-            currentComposer.startProvider(LocalDensity provides Density(this@PlaygroundActivity))
-            rememberVectorPainter(Icons.Default.Favorite).also { currentComposer.endProvider() }
-          }
-        )
-      }
+      test(
+        Modifier
+          .fillMaxSize()
+          .wrapContentSize()
+          .background(Color.Cyan)
+      )
     }
   }
-}
-
-// https://github.com/androidx/androidx/blob/f3ae39e92fbaf02f36011fbe6747aa2aa89da417/compose/runtime/runtime/src/jvmMain/kotlin/androidx/compose/runtime/internal/Expect.jvm.kt#L22
-private fun <T> invokeComposable(composer: Composer, composable: @Composable () -> T): T {
-  @Suppress("UNCHECKED_CAST") val realFn = composable as Function2<Composer, Int, T>
-  return realFn(composer, 1)
 }
