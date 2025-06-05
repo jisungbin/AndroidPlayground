076e9ca179ed6ed2bfeabc8b69b0e4f06ee3e1cf
Ji Sungbin <ji@sungb.in>
2025-06-05 14:32:57 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -1,38 +1,23 @@
-@file:OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimatableApi::class, InternalComposeApi::class)
-
 package land.sungbin.androidplayground
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.ExperimentalAnimatableApi
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.InternalComposeApi
-import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-
-@JvmInline
-value class ComposableClass(
-  val composable: @Composable (Modifier) -> Unit,
-) : @Composable (Modifier) -> Unit by composable
 
 class PlaygroundActivity : ComponentActivity() {
-  private val test = ComposableClass { Text("TTTT", it) }
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      test(
-        Modifier
-          .fillMaxSize()
-          .wrapContentSize()
-          .background(Color.Cyan)
-      )
+      Box {
+        Box(Modifier.align(Alignment.Center))
+        Column {
+          Box(Modifier.align(Alignment.Center))
+        }
+      }
     }
   }
 }
