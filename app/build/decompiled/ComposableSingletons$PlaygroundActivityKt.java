9b7e5ee4ee2b50a51fc9196a589c07a9513159a2
Ji Sungbin <ji@sungb.in>
2025-08-06 21:45:55 +0900

    test

--- app/build/decompiled/ComposableSingletons$PlaygroundActivityKt.java
+++ app/build/decompiled/ComposableSingletons$PlaygroundActivityKt.java
@@ -3,7 +3,6 @@ package land.sungbin.androidplayground;
 import androidx.compose.runtime.internal.ComposableLambdaKt;
 import kotlin.Metadata;
 import kotlin.jvm.functions.Function2;
-import kotlin.jvm.functions.Function4;
 import org.jetbrains.annotations.NotNull;
 
 @Metadata(
@@ -15,14 +14,7 @@ public final class ComposableSingletons$PlaygroundActivityKt {
    @NotNull
    public static final ComposableSingletons$PlaygroundActivityKt INSTANCE = new ComposableSingletons$PlaygroundActivityKt();
    @NotNull
-   private static Function4 lambda$1672179226;
-   @NotNull
    private static Function2 lambda$2023832317;
-
-   @NotNull
-   public final Function4 getLambda$1672179226$app_debug() {
-      return lambda$1672179226;
-   }
 
    @NotNull
    public final Function2 getLambda$2023832317$app_debug() {
@@ -30,7 +22,6 @@ public final class ComposableSingletons$PlaygroundActivityKt {
    }
 
    static {
-      lambda$1672179226 = (Function4)ComposableLambdaKt.composableLambdaInstance(1672179226, false, null.INSTANCE);
       lambda$2023832317 = (Function2)ComposableLambdaKt.composableLambdaInstance(2023832317, false, null.INSTANCE);
    }
 }
--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -17,16 +19,19 @@ class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(100) { it ->
-          println(it)
-          Box(
-            Modifier
-              .fillMaxWidth()
-              .height(50.dp)
-              .background(Color.Cyan),
-          )
-        }
+      Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+      ) {
+        Box(
+          Modifier
+            .size(100.dp)
+            .background(color = Color.Red)
+            .wrapContentHeight()
+            .wrapContentWidth(align = Alignment.End)
+            .size(50.dp)
+            .background(color = Color.Blue),
+        )
       }
     }
   }
