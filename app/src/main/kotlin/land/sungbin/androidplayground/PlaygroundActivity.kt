0ba02dcb4cee30ab714c799b820ab7fb6a4316b3
jisungbin <ji@sungb.in>
2025-08-24 02:40:58 +0900

    test

--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -3,21 +3,27 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.WorkerThread
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import kotlin.reflect.KProperty
 
 class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      val aaa by Unit
-      aaa
+      Main()
     }
   }
 }
 
-@Composable
+@Composable fun Main() {
+  val aaa by Unit
+  aaa
+}
+
+@Composable @WorkerThread @Deprecated("")
 operator fun Unit.getValue(thisRef: Any?, property: KProperty<*>) {
   BasicText(property.name)
 }
