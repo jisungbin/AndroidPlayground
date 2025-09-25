96275cd4f30552329d6231c9ec11c396a4fd8d7a
Ji Sungbin <ji@sungb.in>
2025-09-25 17:38:58 +0900

    test

--- app/build/decompiled/ComposableSingletons$PlaygroundActivityKt.java
+++ app/build/decompiled/ComposableSingletons$PlaygroundActivityKt.java
@@ -1,44 +1,32 @@
 package land.sungbin.androidplayground;
 
-import androidx.compose.foundation.ClickableKt;
-import androidx.compose.foundation.layout.Arrangement;
-import androidx.compose.foundation.layout.ColumnKt;
-import androidx.compose.foundation.layout.ColumnScope;
-import androidx.compose.foundation.layout.ColumnScopeInstance;
+import androidx.compose.foundation.BackgroundKt;
+import androidx.compose.foundation.OverscrollEffect;
+import androidx.compose.foundation.gestures.FlingBehavior;
+import androidx.compose.foundation.layout.BoxKt;
+import androidx.compose.foundation.layout.PaddingKt;
+import androidx.compose.foundation.layout.PaddingValues;
 import androidx.compose.foundation.layout.SizeKt;
-import androidx.compose.foundation.layout.SpacerKt;
-import androidx.compose.foundation.layout.WindowInsetsPadding_androidKt;
-import androidx.compose.foundation.layout.Arrangement.Vertical;
-import androidx.compose.material.TextKt;
-import androidx.compose.runtime.Applier;
+import androidx.compose.foundation.layout.Arrangement.Horizontal;
+import androidx.compose.foundation.lazy.LazyDslKt;
+import androidx.compose.foundation.lazy.LazyItemScope;
+import androidx.compose.foundation.lazy.LazyListScope;
+import androidx.compose.foundation.lazy.LazyListState;
 import androidx.compose.runtime.Composable;
 import androidx.compose.runtime.ComposableTarget;
-import androidx.compose.runtime.ComposablesKt;
 import androidx.compose.runtime.Composer;
 import androidx.compose.runtime.ComposerKt;
-import androidx.compose.runtime.CompositionLocalMap;
-import androidx.compose.runtime.RecomposeScope;
-import androidx.compose.runtime.Updater;
 import androidx.compose.runtime.internal.ComposableLambdaKt;
-import androidx.compose.ui.Alignment;
-import androidx.compose.ui.ComposedModifierKt;
 import androidx.compose.ui.Modifier;
-import androidx.compose.ui.Alignment.Horizontal;
-import androidx.compose.ui.layout.MeasurePolicy;
-import androidx.compose.ui.node.ComposeUiNode;
-import androidx.compose.ui.semantics.Role;
-import androidx.compose.ui.text.TextStyle;
-import androidx.compose.ui.text.font.FontFamily;
-import androidx.compose.ui.text.font.FontStyle;
-import androidx.compose.ui.text.font.FontWeight;
-import androidx.compose.ui.text.style.TextAlign;
-import androidx.compose.ui.text.style.TextDecoration;
+import androidx.compose.ui.Alignment.Vertical;
+import androidx.compose.ui.graphics.Color;
+import androidx.compose.ui.graphics.Shape;
 import androidx.compose.ui.unit.Dp;
 import kotlin.Metadata;
 import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
 import kotlin.jvm.functions.Function1;
 import kotlin.jvm.functions.Function2;
+import kotlin.jvm.functions.Function3;
 import kotlin.jvm.internal.Intrinsics;
 import kotlin.jvm.internal.SourceDebugExtension;
 import org.jetbrains.annotations.NotNull;
@@ -48,137 +36,38 @@ import org.jetbrains.annotations.NotNull;
    k = 3,
    xi = 48
 )
-@SourceDebugExtension({"SMAP\nPlaygroundActivity.kt\nKotlin\n*S Kotlin\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/ComposableSingletons$PlaygroundActivityKt\n+ 2 Column.kt\nandroidx/compose/foundation/layout/ColumnKt\n+ 3 Layout.kt\nandroidx/compose/ui/layout/LayoutKt\n+ 4 Composables.kt\nandroidx/compose/runtime/ComposablesKt\n+ 5 Composer.kt\nandroidx/compose/runtime/Updater\n+ 6 Dp.kt\nandroidx/compose/ui/unit/DpKt\n+ 7 Composer.kt\nandroidx/compose/runtime/ComposerKt\n*L\n1#1,69:1\n87#2:70\n83#2,10:71\n94#2:120\n79#3,6:81\n86#3,3:96\n89#3,2:105\n93#3:119\n347#4,9:87\n356#4:107\n357#4,2:117\n4206#5,6:99\n113#6:108\n113#6:115\n113#6:116\n1282#7,6:109\n*S KotlinDebug\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/ComposableSingletons$PlaygroundActivityKt\n*L\n21#1:70\n21#1:71,10\n21#1:120\n21#1:81,6\n21#1:96,3\n21#1:105,2\n21#1:119\n21#1:87,9\n21#1:107\n21#1:117,2\n21#1:99,6\n23#1:108\n28#1:115\n32#1:116\n26#1:109,6\n*E\n"})
+@SourceDebugExtension({"SMAP\nPlaygroundActivity.kt\nKotlin\n*S Kotlin\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/ComposableSingletons$PlaygroundActivityKt\n+ 2 Dp.kt\nandroidx/compose/ui/unit/DpKt\n+ 3 Composer.kt\nandroidx/compose/runtime/ComposerKt\n*L\n1#1,34:1\n113#2:35\n1282#3,6:36\n*S KotlinDebug\n*F\n+ 1 PlaygroundActivity.kt\nland/sungbin/androidplayground/ComposableSingletons$PlaygroundActivityKt\n*L\n21#1:35\n22#1:36,6\n*E\n"})
 public final class ComposableSingletons$PlaygroundActivityKt {
    @NotNull
    public static final ComposableSingletons$PlaygroundActivityKt INSTANCE = new ComposableSingletons$PlaygroundActivityKt();
    @NotNull
-   private static Function2 lambda$2023832317 = (Function2)ComposableLambdaKt.composableLambdaInstance(2023832317, false, ComposableSingletons$PlaygroundActivityKt::lambda_2023832317$lambda$3);
+   private static Function3 lambda$-1390239302 = (Function3)ComposableLambdaKt.composableLambdaInstance(-1390239302, false, ComposableSingletons$PlaygroundActivityKt::lambda__1390239302$lambda$0);
+   @NotNull
+   private static Function2 lambda$2023832317 = (Function2)ComposableLambdaKt.composableLambdaInstance(2023832317, false, ComposableSingletons$PlaygroundActivityKt::lambda_2023832317$lambda$0);
+
+   @NotNull
+   public final Function3 getLambda$_1390239302$app_debug/* $FF was: getLambda$-1390239302$app_debug*/() {
+      return lambda$-1390239302;
+   }
 
    @NotNull
    public final Function2 getLambda$2023832317$app_debug() {
       return lambda$2023832317;
    }
 
-   private static final Unit lambda_2023832317$lambda$3$lambda$2$lambda$1$lambda$0(RecomposeScope $r) {
-      $r.invalidate();
-      return Unit.INSTANCE;
-   }
-
    @Composable
    @ComposableTarget(
       applier = "androidx.compose.ui.UiComposable"
    )
-   private static final Unit lambda_2023832317$lambda$3(Composer $composer, int $changed) {
-      ComposerKt.sourceInformation($composer, "C20@777L417:PlaygroundActivity.kt#qshby3");
-      if ($composer.shouldExecute(($changed & 3) != 2, $changed & 1)) {
+   private static final Unit lambda__1390239302$lambda$0(LazyItemScope $this$item, Composer $composer, int $changed) {
+      Intrinsics.checkNotNullParameter($this$item, "$this$item");
+      ComposerKt.sourceInformation($composer, "C23@829L112:PlaygroundActivity.kt#qshby3");
+      if ($composer.shouldExecute(($changed & 17) != 16, $changed & 1)) {
          if (ComposerKt.isTraceInProgress()) {
-            ComposerKt.traceEventStart(2023832317, $changed, -1, "land.sungbin.androidplayground.ComposableSingletons$PlaygroundActivityKt.lambda$2023832317.<anonymous> (PlaygroundActivity.kt:20)");
+            ComposerKt.traceEventStart(-1390239302, $changed, -1, "land.sungbin.androidplayground.ComposableSingletons$PlaygroundActivityKt.lambda$-1390239302.<anonymous> (PlaygroundActivity.kt:23)");
          }
 
-         int var6 = 0;
-         int var7 = false;
-         ComposerKt.sourceInformationMarkerStart($composer, -483455358, "CC(Column)P(2,3,1)87@4442L61,88@4508L133:Column.kt#2w3rfo");
-         Modifier var2 = (Modifier)Modifier.Companion;
-         Vertical var3 = Arrangement.INSTANCE.getTop();
-         Horizontal var4 = Alignment.Companion.getStart();
-         MeasurePolicy var8 = ColumnKt.columnMeasurePolicy(var3, var4, $composer, 14 & var6 >> 3 | 112 & var6 >> 3);
-         int var10 = 112 & var6 << 3;
-         int var11 = false;
-         ComposerKt.sourceInformationMarkerStart($composer, -1323940314, "CC(Layout)P(!1,2)79@3206L23,82@3357L359:Layout.kt#80mrfh");
-         int var12 = ComposablesKt.getCurrentCompositeKeyHash($composer, 0);
-         CompositionLocalMap var13 = $composer.getCurrentCompositionLocalMap();
-         Modifier var14 = ComposedModifierKt.materializeModifier($composer, var2);
-         Function0 var15 = ComposeUiNode.Companion.getConstructor();
-         int var16 = 6 | 896 & var10 << 6;
-         int var17 = false;
-         ComposerKt.sourceInformationMarkerStart($composer, -692256719, "CC(ReusableComposeNode)P(1,2)355@14017L9:Composables.kt#9igjgp");
-         if (!($composer.getApplier() instanceof Applier)) {
-            ComposablesKt.invalidApplier();
-         }
-
-         $composer.startReusableNode();
-         if ($composer.getInserting()) {
-            $composer.createNode(var15);
-         } else {
-            $composer.useNode();
-         }
-
-         Composer var18 = Updater.constructor-impl($composer);
-         int var19 = false;
-         Updater.set-impl(var18, var8, ComposeUiNode.Companion.getSetMeasurePolicy());
-         Updater.set-impl(var18, var13, ComposeUiNode.Companion.getSetResolvedCompositionLocals());
-         Function2 var20 = ComposeUiNode.Companion.getSetCompositeKeyHash();
-         int var21 = false;
-         int var23 = false;
-         if (var18.getInserting() || !Intrinsics.areEqual(var18.rememberedValue(), var12)) {
-            var18.updateRememberedValue(var12);
-            var18.apply(var12, var20);
-         }
-
-         Updater.set-impl(var18, var14, ComposeUiNode.Companion.getSetModifier());
-         int var24 = 14 & var16 >> 6;
-         int var26 = false;
-         ComposerKt.sourceInformationMarkerStart($composer, -384672921, "C89@4556L9:Column.kt#2w3rfo");
-         int var27 = 6 | 112 & var6 >> 6;
-         ColumnScope var29 = (ColumnScope)ColumnScopeInstance.INSTANCE;
-         int var30 = false;
-         ComposerKt.sourceInformationMarkerStart($composer, 182746728, "C21@794L47,22@850L40,24@908L21,25@1014L18,25@938L95,27@1043L40,29@1102L9,31@1121L40,33@1177L9:PlaygroundActivity.kt#qshby3");
-         SpacerKt.Spacer(WindowInsetsPadding_androidKt.statusBarsPadding((Modifier)Modifier.Companion), $composer, 0);
-         Modifier var10000 = (Modifier)Modifier.Companion;
-         int var31 = 4;
-         int var32 = false;
-         SpacerKt.Spacer(SizeKt.height-3ABfNKs(var10000, Dp.constructor-impl((float)var31)), $composer, 6);
-         RecomposeScope var47 = ComposablesKt.getCurrentRecomposeScope($composer, 0);
-         String var46 = var47 + " with " + System.currentTimeMillis();
-         Modifier var10001 = (Modifier)Modifier.Companion;
-         boolean var10002 = false;
-         Object var10003 = null;
-         Object var10004 = null;
-         ComposerKt.sourceInformationMarkerStart($composer, -686834939, "CC(remember):PlaygroundActivity.kt#9igjgp");
-         boolean var34 = $composer.changedInstance(var47);
-         int var35 = false;
-         Object var36 = $composer.rememberedValue();
-         int var37 = false;
-         Object var10005;
-         if (!var34 && var36 != Composer.Companion.getEmpty()) {
-            var10005 = var36;
-         } else {
-            Object var38 = null;
-            Object var39 = null;
-            boolean var40 = false;
-            Modifier var41 = var10001;
-            String var42 = var46;
-            int var43 = false;
-            Function0 var44 = ComposableSingletons$PlaygroundActivityKt::lambda_2023832317$lambda$3$lambda$2$lambda$1$lambda$0;
-            var46 = var42;
-            var10001 = var41;
-            var10002 = var40;
-            var10003 = var39;
-            var10004 = var38;
-            $composer.updateRememberedValue(var44);
-            var10005 = var44;
-         }
-
-         Function0 var48 = (Function0)var10005;
-         ComposerKt.sourceInformationMarkerEnd($composer);
-         TextKt.Text--4IGK_g(var46, ClickableKt.clickable-XHw0xAI$default(var10001, var10002, (String)var10003, (Role)var10004, var48, 7, (Object)null), 0L, 0L, (FontStyle)null, (FontWeight)null, (FontFamily)null, 0L, (TextDecoration)null, (TextAlign)null, 0L, 0, false, 0, 0, (Function1)null, (TextStyle)null, $composer, 0, 0, 131068);
-         var10000 = (Modifier)Modifier.Companion;
-         int var49 = 4;
-         int var33 = false;
-         SpacerKt.Spacer(SizeKt.height-3ABfNKs(var10000, Dp.constructor-impl((float)var49)), $composer, 6);
-         TestImpl.INSTANCE.Content(var29, $composer, 48 | 14 & var27);
-         var10000 = (Modifier)Modifier.Companion;
-         var49 = 4;
-         var33 = false;
-         SpacerKt.Spacer(SizeKt.height-3ABfNKs(var10000, Dp.constructor-impl((float)var49)), $composer, 6);
-         Test2.Companion.Content(var29, $composer, 48 | 14 & var27);
-         ComposerKt.sourceInformationMarkerEnd($composer);
-         ComposerKt.sourceInformationMarkerEnd($composer);
-         $composer.endNode();
-         ComposerKt.sourceInformationMarkerEnd($composer);
-         ComposerKt.sourceInformationMarkerEnd($composer);
-         ComposerKt.sourceInformationMarkerEnd($composer);
+         BoxKt.Box(BackgroundKt.background-bw27NRU$default(SizeKt.fillMaxSize$default((Modifier)Modifier.Companion, 0.0F, 1, (Object)null), Color.Companion.getRed-0d7_KjU(), (Shape)null, 2, (Object)null), $composer, 6);
          if (ComposerKt.isTraceInProgress()) {
             ComposerKt.traceEventEnd();
          }
@@ -186,6 +75,81 @@ public final class ComposableSingletons$PlaygroundActivityKt {
          $composer.skipToGroupEnd();
       }
 
+      return Unit.INSTANCE;
+   }
+
+   @Composable
+   @ComposableTarget(
+      applier = "androidx.compose.ui.UiComposable"
+   )
+   private static final Unit lambda_2023832317$lambda$0(Composer $composer, int $changed) {
+      ComposerKt.sourceInformation($composer, "C21@802L157,18@682L277:PlaygroundActivity.kt#qshby3");
+      if ($composer.shouldExecute(($changed & 3) != 2, $changed & 1)) {
+         if (ComposerKt.isTraceInProgress()) {
+            ComposerKt.traceEventStart(2023832317, $changed, -1, "land.sungbin.androidplayground.ComposableSingletons$PlaygroundActivityKt.lambda$2023832317.<anonymous> (PlaygroundActivity.kt:18)");
+         }
+
+         Modifier var10000 = SizeKt.fillMaxSize$default((Modifier)Modifier.Companion, 0.0F, 1, (Object)null);
+         Object var10001 = null;
+         int var2 = 30;
+         int var3 = false;
+         PaddingValues var10002 = PaddingKt.PaddingValues-YgX7TsA$default(Dp.constructor-impl((float)var2), 0.0F, 2, (Object)null);
+         boolean var10003 = false;
+         Object var10004 = null;
+         Object var10005 = null;
+         Object var10006 = null;
+         boolean var10007 = false;
+         Object var10008 = null;
+         ComposerKt.sourceInformationMarkerStart($composer, -717324198, "CC(remember):PlaygroundActivity.kt#9igjgp");
+         boolean var4 = false;
+         int var5 = false;
+         Object var6 = $composer.rememberedValue();
+         int var7 = false;
+         Object var10009;
+         if (var6 == Composer.Companion.getEmpty()) {
+            Object var17 = null;
+            boolean var16 = false;
+            Object var15 = null;
+            Object var14 = null;
+            Object var13 = null;
+            boolean var12 = false;
+            PaddingValues var11 = var10002;
+            Object var10 = null;
+            Modifier var9 = var10000;
+            int var8 = false;
+            Function1 var18 = ComposableSingletons$PlaygroundActivityKt::lambda_2023832317$lambda$0$0$0;
+            var10000 = var9;
+            var10001 = var10;
+            var10002 = var11;
+            var10003 = var12;
+            var10004 = var13;
+            var10005 = var14;
+            var10006 = var15;
+            var10007 = var16;
+            var10008 = var17;
+            $composer.updateRememberedValue(var18);
+            var10009 = var18;
+         } else {
+            var10009 = var6;
+         }
+
+         Function1 var19 = (Function1)var10009;
+         ComposerKt.sourceInformationMarkerEnd($composer);
+         LazyDslKt.LazyRow(var10000, (LazyListState)var10001, var10002, var10003, (Horizontal)var10004, (Vertical)var10005, (FlingBehavior)var10006, var10007, (OverscrollEffect)var10008, var19, $composer, 805306758, 506);
+         if (ComposerKt.isTraceInProgress()) {
+            ComposerKt.traceEventEnd();
+         }
+      } else {
+         $composer.skipToGroupEnd();
+      }
+
+      return Unit.INSTANCE;
+   }
+
+   private static final Unit lambda_2023832317$lambda$0$0$0(LazyListScope $this$LazyRow) {
+      Intrinsics.checkNotNullParameter($this$LazyRow, "$this$LazyRow");
+      ComposableSingletons$PlaygroundActivityKt var10003 = INSTANCE;
+      LazyListScope.item$default($this$LazyRow, (Object)null, (Object)null, lambda$-1390239302, 3, (Object)null);
       return Unit.INSTANCE;
    }
 }
--- app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
+++ app/src/main/kotlin/land/sungbin/androidplayground/PlaygroundActivity.kt
@@ -3,66 +3,31 @@ package land.sungbin.androidplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentRecomposeScope
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class PlaygroundActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      Column {
-        Spacer(modifier = Modifier.statusBarsPadding())
-        Spacer(modifier = Modifier.height(4.dp))
-
-        val r = currentRecomposeScope
-        Text("$r with ${System.currentTimeMillis()}", modifier = Modifier.clickable { r.invalidate() })
-
-        Spacer(modifier = Modifier.height(4.dp))
-
-        TestImpl.Content()
-
-        Spacer(modifier = Modifier.height(4.dp))
-
-        Test2.Content()
+      LazyRow(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 30.dp),
+      ) {
+        item {
+          Box(
+            Modifier
+              .fillMaxSize()
+              .background(color = Color.Red),
+          )
+        }
       }
     }
   }
 }
-
-fun interface Test {
-  context(a: Any)
-  @Composable
-  fun Content()
-}
-
-object TestImpl : Test {
-  context(a: Any)
-  @Composable
-  override fun Content() { // final
-    used(a)
-    val r = currentRecomposeScope
-    Text("$r with ${System.currentTimeMillis()}", modifier = Modifier.clickable { r.invalidate() })
-  }
-}
-
-open class Test2 : Test {
-  context(a: Any)
-  @Composable
-  override fun Content() { // open
-    used(a)
-    val r = currentRecomposeScope
-    Text("$r with ${System.currentTimeMillis()}", modifier = Modifier.clickable { r.invalidate() })
-  }
-
-  companion object : Test2()
-}
-
-fun used(a: Any) {}
